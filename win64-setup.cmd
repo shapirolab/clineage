0db8df9b40996be54d0113a71da907cc4cdacace
Udi Oron <udioron@gmail.com>
2014-05-02 00:24:36 +0300

    Fixed windows setup script

---
+++ win64-setup.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+echo ===============================
+echo Installing lxml
+easy_install lxml==2.3
+
+echo ===============================
+echo Installing MySQL-python
+easy_install MySQL-python==1.2.5
+
+echo ===============================
+echo Installing NumPy
+easy_install http://pkgs.10x.org.il/win64/numpy-MKL-1.8.1.win-amd64-py2.7.exe
