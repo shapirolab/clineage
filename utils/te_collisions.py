795d79ffc8c2534e540db1898478ddbaa670b009
ofirr <ofir.raz@weizmann.ac.il>
2014-06-02 12:18:26 +0300

    updated deletion bug

--- utils/te_collisions.py
+++ utils/te_collisions.py
@@ -2,9 +2,8 @@ __author__ = 'ofirr'
 from linapp.models import Chromosome, TargetEnrichment
 
 def delete_overlapping_tes():
-    tes_to_delete = []
     tes_conflict = []
-
+    tes_bug = []
     c = 0
     t = TargetEnrichment.objects.all().count()
     for chr in Chromosome.objects.all():
@@ -13,29 +12,32 @@ def delete_overlapping_tes():
                                                .filter(left__start_pos__lte=te1.right.end_pos)\
                                                .filter(right__end_pos__gte=te1.left.start_pos)\
                                                .exclude(pk=te1.pk):
-                if te1.physical_locations.all() and not te2.physical_locations.all():
-                    print 'deleted', te2
-                    te2.delete()
-                    continue
-                if te2.physical_locations.all() and not te1.physical_locations.all():
-                    print 'deleted', te1
-                    te1.delete()
-                    continue
-                if te2.physical_locations.all() and te1.physical_locations.all():
-                    if te1.type != te2.type:
+                try:
+                    if te1.physical_locations.all() and not te2.physical_locations.all():
+                        print 'deleted', te2
+                        te2.delete()
+                        continue
+                    if te2.physical_locations.all() and not te1.physical_locations.all():
+                        print 'deleted', te1
+                        te1.delete()
+                        continue
+                    if te2.physical_locations.all() and te1.physical_locations.all():
+                        if te1.type != te2.type:
+                            continue
+                        tes_conflict.append((te1, te2))
+                        continue
+                    if te1.left.start_pos < te2.left.start_pos and te1.right.end_pos > te2.right.end_pos:
+                        print 'deleted', te2
+                        te2.delete()
+                        continue
+                    if te2.left.start_pos < te1.left.start_pos and te2.right.end_pos > te1.right.end_pos:
+                        print 'deleted', te1
+                        te1.delete()
                         continue
                     tes_conflict.append((te1, te2))
-                    continue
-                if te1.left.start_pos < te2.left.start_pos and te1.right.end_pos > te2.right.end_pos:
-                    print 'deleted', te2
-                    te2.delete()
-                    continue
-                if te2.left.start_pos < te1.left.start_pos and te2.right.end_pos > te1.right.end_pos:
-                    print 'deleted', te1
-                    te1.delete()
-                    continue
-                tes_conflict.append((te1, te2))
+                except AssertionError:
+                    tes_bug.append((te1,te2))
             c += 1
             if c % 100 == 0:
-                print len(tes_to_delete), len(tes_conflict), c, float(c)/t*100
-    return tes_conflict
+                print len(tes_conflict), len(tes_bug), c, float(c)/t*100
+    return tes_conflict, tes_bug
