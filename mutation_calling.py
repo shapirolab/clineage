16b1f61d8b0bc84cc628c99ef56c47f339acfe58
ofirr <ofir.raz@weizmann.ac.il>
2014-12-24 21:01:35 +0200

    Added more input parameters

--- mutation_calling.py
+++ mutation_calling.py
@@ -23,6 +23,13 @@ if '__main__' == __name__:
     parser.add_argument('-sf', '--simulationsfile', type=str, dest='simulationsfile', default='sim_hists.pickle', help='path of preprocessed simulations file')
     parser.add_argument('-cf', '--callingfile', type=str, dest='callingfile', default='calling.pickle', help='path of preprocessed calling file')
     parser.add_argument('-sm', '--shiftmargins', type=int, dest='shiftmargins', default=15, help='number of attempted shifts to either side of the histogram median')
+
+    #TODO: document those:
+    parser.add_argument('-ma', '--maxalleles', type=int, dest='max_alleles', default=2, help='number of attempted shifts to either side of the histogram median')
+    parser.add_argument('-sd', '--sampledepth', type=int, dest='sample_depth', default=10000, help='number of attempted shifts to either side of the histogram median')
+    parser.add_argument('-ml', '--maxmslength', type=int, dest='max_ms_length', default=60, help='number of attempted shifts to either side of the histogram median')
+    parser.add_argument('-md', '--mediandistance', type=int, dest='max_distance_from_median', default=10, help='number of attempted shifts to either side of the histogram median')
+
     
     args = parser.parse_args()
     normalize = args.normalize
@@ -36,6 +43,10 @@ if '__main__' == __name__:
     cycles_threshold = args.maxcycles
     min_cycles = args.mincycles
     shift_margins = args.shiftmargins
+    max_alleles = args.max_alleles
+    sample_depth = args.sample_depth
+    max_ms_length = args.max_ms_length
+    max_distance_from_median = args.max_distance_from_median
     meth = args.method
     verbose = args.verbose
     SIMULATED_HISTS_PATH = args.simulationsfile
@@ -46,13 +57,13 @@ if '__main__' == __name__:
                                                 max_cycles=cycles_threshold,
                                                 up=lambda d: 0.00005*d**2 - 0.0009*d + 0.0036,
                                                 dw=lambda d: 0.00009*d**2 - 0.00003*d - 0.0013,
-                                                max_ms_length=60,
-                                                sample_depth=10000,
-                                                max_alleles=2,
-                                                normalize=True,
-                                                truncate=False,
-                                                cut_peak=False,
-                                                trim_extremes=False)
+                                                max_ms_length=max_ms_length,
+                                                sample_depth=sample_depth,
+                                                max_alleles=max_alleles,
+                                                normalize=normalize,
+                                                truncate=truncate,
+                                                cut_peak=cutpeak,
+                                                trim_extremes=trim_extremes)
     calling = load_or_create_calling(SIGNALS_CALLING_PATH)
     
     calling = generate_calling_file(input_file,
@@ -69,15 +80,28 @@ if '__main__' == __name__:
                                     cut_peak=cutpeak,
                                     trim_extremes=trim_extremes,
                                     shift_margins=shift_margins,
-                                    max_distance_from_median=10,
-                                    max_alleles=2,
-                                    max_ms_length=60
+                                    max_distance_from_median=max_distance_from_median,
+                                    max_alleles=max_alleles,
+                                    max_ms_length=max_ms_length
                                     )
 
     save_calling_file(calling, SIGNALS_CALLING_PATH)
     generate_output_file(input_file,
                          output_file,
                          calling,
+                         sim_hists,
+                         method=meth,
                          reads_threshold=reads_threshold,
                          score_threshold=score_threshold,
+                         min_cycles=min_cycles,
+                         max_cycles=cycles_threshold,
+                         nsamples=None,
+                         normalize=normalize,
+                         truncate=truncate,
+                         cut_peak=cutpeak,
+                         trim_extremes=trim_extremes,
+                         shift_margins=shift_margins,
+                         max_distance_from_median=max_distance_from_median,
+                         max_alleles=max_alleles,
+                         max_ms_length=max_ms_length,
                          verbose=verbose)
