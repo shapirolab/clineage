637807148fecfd2b1209630ad806642d8a71c35b
ofirr <ofir.raz@weizmann.ac.il>
2014-12-24 21:06:29 +0200

    Added dynamic histograms simulation

--- order/binomial_sim.py
+++ order/binomial_sim.py
@@ -1,6 +1,27 @@
 import pacal
+from collections import defaultdict
+from order.hist import Histogram
+
 
 def sim(g, up, dw):
-	x = pacal.BinomialDistr(g, up)
-	y = pacal.BinomialDistr(g, dw)
-	return x-y
+    x = pacal.BinomialDistr(g, up)
+    y = pacal.BinomialDistr(g, dw)
+    return x-y
+
+
+def update_probs(cycle, up, dw, probs):
+    if cycle <= 0:
+        return probs
+    new_probs = defaultdict(float)
+    for key in probs.keys():
+        up_p = up(key)
+        dw_p = dw(key)
+        new_probs[key] += (1-up_p-dw_p)*probs[key]
+        new_probs[key-1] += dw_p*probs[key]
+        new_probs[key+1] += up_p*probs[key]
+    return update_probs(cycle-1, up, dw, Histogram(new_probs, normalize=False, nsamples=1, truncate=False, cut_peak=False, trim_extremes=False) + probs)
+        
+    
+def dyn_prob(cycle, d, up, dw):
+    hist = Histogram({d:1.0}, normalize=False, nsamples=1, truncate=False, cut_peak=False, trim_extremes=False)
+    return update_probs(cycle, up, dw, hist)
