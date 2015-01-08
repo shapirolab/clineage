a202333b105be9e156a4a506f76265ddf3d5bcc2
ofirr <ofir.raz@weizmann.ac.il>
2015-01-08 10:14:57 +0200

    Removed commented-out input file parser and rewrote it as a generator

--- order/utils/parsers.py
+++ order/utils/parsers.py
@@ -1,6 +1,6 @@
 import csv
 import StringIO
-
+import gzip
 
 def parse_spaces_hist(hist, header):
     f = StringIO.StringIO('{}\r\n{}'.format(header.strip(), hist.strip()))
@@ -9,26 +9,26 @@ def parse_spaces_hist(hist, header):
     return {int(key) : int(row[key]) for key in row.keys()}
 
 
-# def parse_input_file(input_file,
-#                       sim_hists,
-#                       calling,
-#                       method='cor',
-#                       reads_threshold=50,
-#                       score_threshold=0.006,
-#                       min_cycles=0,
-#                       max_cycles=80,
-#                       normalize=True,
-#                       truncate=False,
-#                       cutpeak=False,
-#                       trim_extremes=False):
-#     with gzip.open(input_file, 'rb') as f:
-#         rdr = csv.reader(f, dialect='excel-tab')
-#         header_row = rdr.next()
-#         for row in bar(rdr):
-#             row_hist = parse_spaces_hist(row[2], header_row[2])
-#             loc = row[0]
-#             cell = row[1]
-#             if sum(row_hist.values()) < reads_threshold:
-#                 continue
-#             vc = get_or_create_call(row_hist, loc, cell, calling, sim_hists, min_cycles=min_cycles, max_cycles=max_cycles, method=method, shift_margines=3, normalize=True, nsamples=None, trunc=truncate, cut_peak=cutpeak, trim_extremes=trim_extremes)
-#     return calling
+def parse_input_file(input_file):
+    """
+    :param input_file:
+    :param reads_threshold:
+    :return:
+    """
+    with gzip.open(input_file, 'rb') as f:
+        rdr = csv.reader(f, dialect='excel-tab')
+        header_row = rdr.next()
+        for row in rdr:
+            row_hist = parse_spaces_hist(row[2], header_row[2])
+            loc = row[0]
+            cell = row[1]
+            yield loc, cell, row_hist
+
+
+def uncalled_inputs(input_file, calling, reads_threshold=0):
+    for loc, cell, row_hist in parse_input_file(input_file):
+        if calling[loc][cell]:
+            continue
+        if sum(row_hist.values()) < reads_threshold:
+            continue
+        yield loc, cell, row_hist
