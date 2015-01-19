3f7d19b4956d8566ac2cfbce944a50bcdd12e892
ofirr <ofir.raz@weizmann.ac.il>
2015-01-19 16:40:56 +0200

    Changed parser to return Histogram objects. Left a function for raw dicts.

--- experimental_data/synthetic_data.py
+++ experimental_data/synthetic_data.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
 import csv
+from order.hist import Histogram
 
-
-def read_synthetic_data(filename='hist_by_ms_len_as_0_sum.tab'):
+def read_synthetic_data_raw(filename='hist_by_ms_len_as_0_sum.tab'):
     hist_per_length_per_cycle = defaultdict(lambda: defaultdict(list))
     with open(filename, 'rb') as f:
         rdr = csv.DictReader(f, dialect='excel-tab')
@@ -10,6 +10,16 @@ def read_synthetic_data(filename='hist_by_ms_len_as_0_sum.tab'):
             hist_values = {key: int(row[str(key)]) for key in range(3, 100)}
             hist_per_length_per_cycle[int(row['len'])][int(row['cycles'])] = hist_values
         return hist_per_length_per_cycle
+
+
+def read_synthetic_data(filename='hist_by_ms_len_as_0_sum.tab', normalize=False, nsamples=None, trunc=False, cut_peak=False):
+    hist_per_length_per_cycle = read_synthetic_data_raw(filename)
+    hist_object_per_length_per_cycle = defaultdict(lambda: defaultdict(list))
+    for ms_len in hist_per_length_per_cycle:
+        for cycle in hist_per_length_per_cycle[ms_len]:
+            hist_object = Histogram(hist_per_length_per_cycle[ms_len][cycle], normalize=normalize, nsamples=nsamples, trunc=trunc, cut_peak=cut_peak)
+            hist_object_per_length_per_cycle[ms_len][cycle] = hist_object
+    return hist_object_per_length_per_cycle
 
 
 def get_hists_pairs_by_len(hist_per_length_per_cycle):
