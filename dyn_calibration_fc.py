836de5fb204f700adfbec349172dbee94d4f0d25
ofirr <ofir.raz@weizmann.ac.il>
2015-01-21 18:49:05 +0200

    replaced inner fitness function with external one from scoring.py.

--- dyn_calibration_fc.py
+++ dyn_calibration_fc.py
@@ -6,31 +6,23 @@ from time import sleep, time
 
 import numpy
 from experimental_data.synthetic_data import get_hists_pairs, get_transposed_hists_pairs
-from order.calibration.score import distance_from_model
-
+from order.calibration.score import distance_from_model, distance_from_model_across_lengths
 
 
 def doWork(input_tuple):
     alg, optimizer_method, hist_pairs, cycles_tup = input_tuple
     cycles_26, cycles_47 = cycles_tup
-    def mes(up, dw):
-        score = 0.0
-        for syn_len, syn_hist_26, syn_hist_47 in hist_pairs:
-            msmodel = (up, dw, 'bin')
-            score += distance_from_model(syn_len, syn_hist_26, cycles_26, msmodel, distance_measure='con')
-            score += distance_from_model(syn_len, syn_hist_47, cycles_47, msmodel, distance_measure='con')
-        return score
-
     def nmes(x):
         assert len(x) % 2 == 0
         up_params = list(x)[:len(x)/2]
         dw_params = list(x)[len(x)/2:]
         up = numpy.poly1d(up_params)
         dw = numpy.poly1d(dw_params)
-        return mes(up, dw)
+        msmodel = (up, dw, optimizer_method)
+        return distance_from_model_across_lengths(msmodel, hist_pairs, cycles_tup, distance_measure=alg)
 
     minimizer_kwargs = dict(method=optimizer_method, bounds=[(0, 0.1), (-0.1, 0.1), (-0.5, 0.5), (0, 0.1), (-0.1, 0.1), (-0.5, 0.5)], options={'eps' : 1e-3, 'disp' : True})
-    res = optimize.basinhopping(nmes, [0.00005,  -0.0009, 0.0036, 0.00009, -0.00003, - .0013], minimizer_kwargs=minimizer_kwargs, niter=100)
+    res = optimize.basinhopping(nmes, [0.00005,  -0.0009, 0.0036, 0.00009, -0.00003, -0.0013], minimizer_kwargs=minimizer_kwargs, niter=100)
     return res
 
 
