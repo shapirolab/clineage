02f72002d961244c7bcc15ede07bce58abe32af6
ofirr <ofir.raz@weizmann.ac.il>
2015-01-21 19:43:46 +0200

    generalized the optimization function and moved it to calibration.score

--- order/calibration/score.py
+++ order/calibration/score.py
@@ -33,3 +33,19 @@ def distance_from_model_across_lengths(msmodel, syn_hist_list, cycles_pair, dist
         score += distance_from_model(syn_len, syn_hist_26, cycles_26, msmodel, distance_measure=distance_measure, **kwargs)
         score += distance_from_model(syn_len, syn_hist_47, cycles_47, msmodel, distance_measure=distance_measure, **kwargs)
     return score
+
+
+def optimize_across_lengths(input_tuple):
+    alg, optimizer_method, hist_pairs, cycles_tup, bounds, initial_guess, iterations, optimizer_options = input_tuple
+    def nmes(x):
+        assert len(x) % 2 == 0
+        up_params = list(x)[:len(x)/2]
+        dw_params = list(x)[len(x)/2:]
+        up = numpy.poly1d(up_params)
+        dw = numpy.poly1d(dw_params)
+        msmodel = (up, dw, optimizer_method)
+        return distance_from_model_across_lengths(msmodel, hist_pairs, cycles_tup, distance_measure=alg)
+
+    minimizer_kwargs = dict(method=optimizer_method, bounds=bounds, options=optimizer_options)
+    res = optimize.basinhopping(nmes, initial_guess, minimizer_kwargs=minimizer_kwargs, niter=iterations)
+    return res
