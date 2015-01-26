d8814aec5d429792d188747827202774bca9f8e5
ofirr <ofir.raz@weizmann.ac.il>
2015-01-26 11:21:09 +0200

    Removed non-trivial parallelization dependency + cosmetics

--- dyn_calibration_fc.py
+++ dyn_calibration_fc.py
@@ -1,6 +1,4 @@
 import itertools
-import concurrent.futures
-
 from experimental_data.synthetic_data import get_hists_pairs, get_transposed_hists_pairs
 from order.calibration.score import optimize_across_lengths
 
@@ -20,21 +18,15 @@ def inputs_generator():
     # optimizer_method = "TNC"
     # optimizer_method = "L-BFGS-B"
     for c1, c2 in itertools.product(range(10, 13, 1), range(42, 46, 1)):
-    # for c1, c2 in itertools.product(range(11, 12, 1), range(38, 39, 1)):
         if c2 > c1:
             yield alg, sim, optimizer_method, hist_pairs, (c1, c2), bounds, initial_guess, iterations, optimizer_options
 
 
 if __name__ == '__main__':
-    # pool = Pool(processes=23)
-    # rs = pool.map_async(doWork, inputs_generator(), chunksize=1)
-    # print len(list(inputs_generator()))
-    # rs = map(optimize_across_lengths, inputs_generator())
-    # print rs
     results = []
     with concurrent.futures.ProcessPoolExecutor(max_workers=12) as executor:
-        # for result in map(optimize_across_lengths, inputs_generator()):
-        for result in executor.map(optimize_across_lengths, inputs_generator()):
+        for result in map(optimize_across_lengths, inputs_generator()):
+
             results.append(result)
             alg, sim, optimizer_method, cycles_tup, ms_lengths, res = result
             print alg, sim, optimizer_method, cycles_tup, ms_lengths, res
