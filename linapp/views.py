a3fb84a72d9c06ef7846a2800b59215ee121b641
niki_main <niki_berman@bitbucket.com>
2015-03-15 17:09:03 +0200

    test views partner_cells_table_view request

--- linapp/views.py
+++ linapp/views.py
@@ -746,28 +746,29 @@ def longview(request):
 def partner_cells_table_view(request, partner_name,
                              cell_folder='/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR2/fastq_human/Output',
                              individual_name=None):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="{}/{}_cell_data.csv".format(cell_folder, partner_name)'
-    fieldnames = ['Cell ID',
-                  'Cell Name',
-                  'Cell Type',
-                  'Plate',
-                  'Well',
-                  'Group Color',
-                  'Sampling Event',
-                  'Sampling Comment',
-                  'Organ',
-                  'Tissue',
-                  'Sample Name',
-                  'Sample Comment',
-                  'Individual Name',
-                  'Individual Comment',
-                  'Gender',
-                  'Sequencing File Name',
-                  ]
-    writer = csv.DictWriter(response, fieldnames=fieldnames)
-    writer.writeheader()
-    print '############niki################'
-    for cell_values in user_cells_table_values(partner_name, individual_name, cell_folder):
-            writer.writerow(cell_values)
-    return response
+    # response = HttpResponse(content_type='text/csv')
+    # response['Content-Disposition'] = 'attachment; filename="{}/{}_cell_data.csv".format(cell_folder, partner_name)'
+    # fieldnames = ['Cell ID',
+    #               'Cell Name',
+    #               'Cell Type',
+    #               'Plate',
+    #               'Well',
+    #               'Group Color',
+    #               'Sampling Event',
+    #               'Sampling Comment',
+    #               'Organ',
+    #               'Tissue',
+    #               'Sample Name',
+    #               'Sample Comment',
+    #               'Individual Name',
+    #               'Individual Comment',
+    #               'Gender',
+    #               'Sequencing File Name',
+    #               ]
+    # writer = csv.DictWriter(response, fieldnames=fieldnames)
+    # writer.writeheader()
+    # print '############niki################'
+    # for cell_values in user_cells_table_values(partner_name, individual_name, cell_folder):
+    #         writer.writerow(cell_values)
+    # return response
+    return HttpResponse(str(list(user_cells_table_values(partner_name, individual_name, cell_folder))), content_type="text/plain")
