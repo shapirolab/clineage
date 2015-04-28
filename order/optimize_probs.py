641132bee662cbef8911fb6cc8e5879303529344
ofirr <ofir.raz@weizmann.ac.il>
2015-04-28 18:19:54 +0300

    Rewrote the markovian matrix to accept a list of polynomes. One for every step

--- order/optimize_probs.py
+++ order/optimize_probs.py
@@ -1,17 +1,47 @@
 from numpy.linalg import matrix_power
 from numpy import matrix
+from scipy.stats import binom
+from collections import defaultdict
+import numpy as np
 
-def markovian_matrix(fu, fd, n=100):
+# multiple up/ multiple dw implementation
+def markovian_matrix(fus, fds, n=100):
+    """
+    :param fus: list of fu probability functions from +1 to +n
+    :param fds: list of fd probability functions from -1 to -n
+    :param n:
+    :return:
+    """
+    assert len(fus) <= n/2
+    assert len(fds) <= n/2
+
     def fs(n):
-        return 1-fu(n)-fd(n)
-    return matrix([[fd(0)+fs(0),fu(0)] + [0]*(n-2)] + 
-                  [[0]*(i-1) + [fd(i), fs(i), fu(i)] + [0]*(n-i-2)
-                    for i in xrange(1,n-1)] + 
-                  [[0]*(n-2) + [fd(n), fs(n)+fu(n)]])
+        return 1-sum([fu(n) for fu in fus])-sum([fd(n) for fd in fds])
+
+    l = []
+    for i in xrange(len(fds)):  # first rows with fds squeezing
+        l.append(
+            [fd(i) for fd in reversed(fds[:i])] +
+            [sum([fd(i) for fd in fds[i:]])+fs(i)] +
+            [fu(i) for fu in fus] + [0]*(n-i-len(fus)-1)
+        )
+    for i in xrange(len(fds), n-len(fus)):  # intermediate rows without squeezing
+        l.append([0]*(i-len(fds)) +
+                 [fd(i) for fd in reversed(fds)] +
+                 [fs(i)] +
+                 [fu(i)for fu in fus] +
+                 [0]*(n-i-len(fus)-1))
+
+    for fui, i in enumerate(xrange(n-len(fus)+1, n+1)):  # last rows with fus squeezing
+        l.append([0]*(n-len(fds)-(len(fus)-fui)) + [fd(i) for fd in reversed(fds)] +
+                 [fs(n) + sum([fu(i) for fu in fus[len(fus)-fui-1:]])] +
+                 [fu(i) for fu in fus[:len(fus)-fui-1]])
+    return matrix(l)
+
 
 def get_probs_n(mat, n):
-    return [mat[n,i] for i in xrange(mat.shape[0])]
+    return [mat[n, i] for i in xrange(mat.shape[0])]
 
 
 def dyn_mat_model(fu, fd, ms_len, cycles):
-    return get_probs_n(matrix_power(markovian_matrix(fu, fd), cycles),ms_len)
+    return get_probs_n(matrix_power(markovian_matrix(fu, fd), cycles), ms_len)
