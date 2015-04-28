ea4a28786da045284d8845255a1d00d7f9ae7a5a
ofirr <ofir.raz@weizmann.ac.il>
2015-04-28 18:33:32 +0300

    Added parameters handling to allow multi-polinome input

--- order/calibration/score.py
+++ order/calibration/score.py
@@ -37,18 +37,31 @@ def distance_from_model_across_lengths(msmodel, syn_hist_list, cycles_pair, dist
     for syn_len, syn_hist_26, syn_hist_47 in syn_hist_list:
         score += distance_from_model(syn_len, syn_hist_26, cycles_26, msmodel, distance_measure=distance_measure, **kwargs)
         score += distance_from_model(syn_len, syn_hist_47, cycles_47, msmodel, distance_measure=distance_measure, **kwargs)
+    # print msmodel, score
     return score
+
+
+def get_x_from_list(l, step):
+    assert len(l) % step == 0
+    for i in xrange(0, len(l), step):
+        res = []
+        for j in xrange(step):
+            res.append(l[i+j])
+        yield tuple(res)
 
 
 def optimize_across_lengths(input_tuple):
     alg, sim, optimizer_method, hist_pairs, cycles_tup, bounds, initial_guess, iterations, optimizer_options = input_tuple
+
     def nmes(x):
         assert len(x) % 2 == 0
-        up_params = list(x)[:len(x)/2]
-        dw_params = list(x)[len(x)/2:]
-        up = numpy.poly1d(up_params)
-        dw = numpy.poly1d(dw_params)
-        msmodel = (up, dw, sim)
+        up_params = list(x)[:len(x)/4]
+        dw_params = list(x)[len(x)/4:]
+        # ups = [numpy.poly1d([a, b, c]) for a, b, c in get_x_from_list(up_params, 3)]
+        # dws = [numpy.poly1d([a, b, c]) for a, b, c in get_x_from_list(dw_params, 3)]
+        ups = [numpy.poly1d([a, b]) for a, b in get_x_from_list(up_params, 2)]
+        dws = [numpy.poly1d([a, b]) for a, b in get_x_from_list(dw_params, 2)]
+        msmodel = (ups, dws, sim)
         return distance_from_model_across_lengths(msmodel, hist_pairs, cycles_tup, distance_measure=alg)
 
     minimizer_kwargs = dict(method=optimizer_method, bounds=bounds, options=optimizer_options)
