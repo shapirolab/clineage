85096270c214f28215d0f207f4a6b942aad4eaa4
ofirr <ofir.raz@weizmann.ac.il>
2015-06-22 15:28:13 +0300

    Added aggregation of multiple calling inputs in order to minimize communication overhead

--- cluster_run.py
+++ cluster_run.py
@@ -1,23 +1,21 @@
 import os
-os.chdir('/home/ofirr/s/Ofir/fresh_histcalling/histcalling')
-from order.calling import load_or_create_calling
-from order.utils.output_formatters import generate_calling_file, \
-    generate_output_file, \
-    load_or_create_simulations_file, \
-    save_calling_file
+os.chdir('/home/ofirr/s/Ofir/hcc/hist_calling')
+from order.utils.output_formatters import load_or_create_calling
+from order.utils.output_formatters import generate_output_file, load_or_create_simulations_file, save_calling_file
 import numpy
 from frogress import bar
 from IPython.parallel import Client
 from itertools import izip, repeat
+import itertools
 
 normalize = True
 truncate = False
 cutpeak = False
 trim_extremes = False
-# output_file = 's/LINEAGE/Hiseq/NSR3/fastq_human/Calling/NSR3_AC_nonX_bin_1a.tab.gz'
-output_file = 's/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ML76_AC_X_mat_1a.tab.gz'
-# input_file = 's/LINEAGE/Hiseq/NSR3/fastq_human/Calling/input_hists_AC_nonX.tab.gz'
-input_file = 's/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/input_hists_AC_X.tab.gz'
+# output_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/NSR3_AC_nonX_bin_1a.tab.gz'
+output_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ML76_AC_X_mat_1a.tab.gz'
+# input_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/input_hists_AC_nonX.tab.gz'
+input_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/input_hists_AC_X.tab.gz'
 reads_threshold = 30
 score_threshold = 999.9
 cycles_threshold = 60
@@ -30,8 +28,8 @@ max_distance_from_median = 3
 meth = 'con'
 verbose = True
 sim_method = 'mat'
-SIMULATED_HISTS_PATH = 's/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ac_mat_1a_sim_hists_15o.pickle'
-SIGNALS_CALLING_PATH = 's/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ML76_AC_X_mat_1a.pickle'
+SIMULATED_HISTS_PATH = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ac_mat_1a_sim_hists_15o.pickle'
+SIGNALS_CALLING_PATH = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ML76_AC_X_mat_1a.pickle'
 
 # AC #
 ups = [numpy.poly1d([0.00026892, -0.00205025])]
@@ -58,7 +56,7 @@ rc = Client()
 dview = rc[:]
 with dview.sync_imports():
     import os
-dview.execute("os.chdir('/home/ofirr/s/Ofir/fresh_histcalling/hist_calling/')")
+dview.execute("os.chdir('/home/ofirr/s/Ofir/hcc/hist_calling/')")
 with dview.sync_imports():
     from order.calling import call_multi_hist, inflate_index, flatten_index, uncalled_inputs
 
@@ -67,15 +65,24 @@ lview.block = False
 cpr = dview.use_cloudpickle()
 
 
-def helper(tup):
-    input_tup, extras = tup
-    loc, cell, row_hist = input_tup
-    flat_sim_hists, kwargs = extras
-    sim_hists = inflate_index(flat_sim_hists)
-    # print 'working on loc: {} , cell: ...{}'.format(loc, cell[-15:])
-    res = call_multi_hist(row_hist, sim_hists, **kwargs)
-    return loc, cell, row_hist, res
+def grouper(n, iterable):
+    it = iter(iterable)
+    while True:
+       chunk = tuple(itertools.islice(it, n))
+       if not chunk:
+           return
+       yield chunk
 
+
+def helper(input_tup):
+    results = []
+    uncalled_inputs_group, extras = input_tup
+    for loc, cell, row_hist in uncalled_inputs_group:
+        flat_sim_hists, kwargs = extras
+        sim_hists = inflate_index(flat_sim_hists)
+        res = call_multi_hist(row_hist, sim_hists, **kwargs)
+        results.append((loc, cell, row_hist, res))
+    return results
 
 kwargs = {'method': meth,
           'score_threshold': score_threshold,
@@ -91,16 +98,15 @@ kwargs = {'method': meth,
           'max_alleles': max_alleles,
           'max_ms_length': max_ms_length}
 
+
+uncalled_inputs_groups = grouper(10000, uncalled_inputs(input_file, calling, reads_threshold=reads_threshold))
 flat_sim_hists = flatten_index(sim_hists)
-for result in bar(lview.map(helper,
-                            izip(
-                                uncalled_inputs(input_file,
-                                                calling,
-                                                reads_threshold=reads_threshold),
-                                repeat((flat_sim_hists, kwargs))
-                            ))):
-    loc, cell, row_hist, res = result
-    calling[loc][cell] = res
+inputs_generator = izip(uncalled_inputs_groups, repeat((flat_sim_hists, kwargs)))
+
+for results in bar(lview.map(helper, inputs_generator)):
+    for result in results:
+        loc, cell, row_hist, res = result
+        calling[loc][cell] = res
 
 
 save_calling_file(calling, SIGNALS_CALLING_PATH)
