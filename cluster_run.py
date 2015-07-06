ef9ffc47398430673dc5f992e49e2a6847ece75d
ofirr <ofir.raz@weizmann.ac.il>
2015-07-06 10:05:41 +0300

    running file progress and tweaks

--- cluster_run.py
+++ cluster_run.py
@@ -1,35 +1,36 @@
 import os
-os.chdir('/home/ofirr/s/Ofir/hcc/hist_calling')
+# os.chdir('/home/ofirr/s/Ofir/hcc/hist_calling')
+os.chdir('/home/ofirr/s/Ofir/fresh_histcalling/hist_calling')
 from order.utils.output_formatters import load_or_create_calling
 from order.utils.output_formatters import generate_output_file, load_or_create_simulations_file, save_calling_file
 import numpy
 from frogress import bar
-from IPython.parallel import Client
 from itertools import izip, repeat
 import itertools
+from order.utils.parsers import parse_input_file
+# from IPython.parallel import Client
 
 normalize = True
 truncate = False
 cutpeak = False
 trim_extremes = False
-# output_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/NSR3_AC_nonX_bin_1a.tab.gz'
-output_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ML76_AC_X_mat_1a.tab.gz'
-# input_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/input_hists_AC_nonX.tab.gz'
-input_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/input_hists_AC_X.tab.gz'
+output_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/NSR3_AC_nonX_mat_2a_prop.tab.gz'
+input_file = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/input_hists_AC_nonX.tab.gz'
 reads_threshold = 30
 score_threshold = 999.9
-cycles_threshold = 60
-min_cycles = 20
-shift_margins = 8
-max_alleles = 1
+cycles_threshold = 50
+min_cycles = 30
+shift_margins = 16
+max_alleles = 2
 sample_depth = 10000
-max_ms_length = 50
-max_distance_from_median = 3
-meth = 'con'
+min_ms_length = 5
+max_ms_length = 35
+max_distance_from_median = 8
+meth = 'emd'
 verbose = True
 sim_method = 'mat'
-SIMULATED_HISTS_PATH = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ac_mat_1a_sim_hists_15o.pickle'
-SIGNALS_CALLING_PATH = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Miseq/ML76/Unified/fastq/Output/Calling/ML76_AC_X_mat_1a.pickle'
+SIMULATED_HISTS_PATH = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/ac_mat_2a_prop_sim_hists_with_replacement.pickle'
+SIGNALS_CALLING_PATH = '/net/mraid11/export/data/dcsoft/home/LINEAGE/Hiseq/NSR3/fastq_human/Calling/NSR3_AC_nonX_mat_2a_prop.pickle'
 
 # AC #
 ups = [numpy.poly1d([0.00026892, -0.00205025])]
@@ -39,9 +40,11 @@ dws = [numpy.poly1d([0.00191615, -0.01174076]),
 
 sim_hists = load_or_create_simulations_file(SIMULATED_HISTS_PATH,
                                             method=sim_method,
+                                            min_cycles=min_cycles,
                                             max_cycles=cycles_threshold,
                                             ups=ups,
                                             dws=dws,
+                                            min_ms_length=min_ms_length,
                                             max_ms_length=max_ms_length,
                                             sample_depth=sample_depth,
                                             max_alleles=max_alleles,
@@ -78,11 +81,13 @@ def helper(input_tup):
     results = []
     uncalled_inputs_group, extras = input_tup
     for loc, cell, row_hist in uncalled_inputs_group:
-        flat_sim_hists, kwargs = extras
-        sim_hists = inflate_index(flat_sim_hists)
-        res = call_multi_hist(row_hist, sim_hists, **kwargs)
+        # flat_sim_hists, kwargs = extras
+        sim_hists, kwargs = extras
+        # sim_hists = inflate_index(flat_sim_hists)
+        res = call_multi_hist(row_hist, sim_hists, proportional=True, **kwargs)
         results.append((loc, cell, row_hist, res))
     return results
+
 
 kwargs = {'method': meth,
           'score_threshold': score_threshold,
@@ -99,11 +104,27 @@ kwargs = {'method': meth,
           'max_ms_length': max_ms_length}
 
 
-uncalled_inputs_groups = grouper(10000, uncalled_inputs(input_file, calling, reads_threshold=reads_threshold))
-flat_sim_hists = flatten_index(sim_hists)
-inputs_generator = izip(uncalled_inputs_groups, repeat((flat_sim_hists, kwargs)))
+def uncalled_inputs_for_loc(input_file, calling, locs_filter, reads_threshold=0):
+    for loc, cell, row_hist in parse_input_file(input_file):
+        if loc not in locs_filter:
+            continue
+        if calling[loc][cell]:
+            continue
+        if sum(row_hist.values()) < reads_threshold:
+            continue
+        yield loc, cell, row_hist
 
-for results in bar(lview.map(helper, inputs_generator)):
+
+# uncalled_inputs_groups = grouper(10000, uncalled_inputs(input_file, calling, reads_threshold=reads_threshold))
+# uncalled_inputs_groups = grouper(1, uncalled_inputs_for_loc(input_file, calling, locs_filter=['62531:8_87865362_87865421'], reads_threshold=reads_threshold))
+uncalled_inputs_groups = grouper(1, uncalled_inputs_for_loc(input_file, calling, locs_filter=['29948:15_40846105_40846166'], reads_threshold=reads_threshold))
+
+# flat_sim_hists = flatten_index(sim_hists)
+# inputs_generator = izip(uncalled_inputs_groups, repeat((flat_sim_hists, kwargs)))
+inputs_generator = izip(uncalled_inputs_groups, repeat((sim_hists, kwargs)))
+
+for results in bar(map(helper, inputs_generator)):
+# for results in bar(lview.map(helper, inputs_generator)):
     for result in results:
         loc, cell, row_hist, res = result
         calling[loc][cell] = res
