c0b63e1b6003edb1ee85d11e5cc6269b45bb696a
ofirr <ofir.raz@weizmann.ac.il>
2015-07-06 09:59:46 +0300

    Added two more implementations of KS distance between histograms

--- order/hist_dist.py
+++ order/hist_dist.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy import stats
 from emd import emd
 from math import sqrt, log10
 from hist import get_lims
@@ -42,7 +42,31 @@ def pop_dist_ks_2samp(hist1, hist2, reads, sample_depth):
     """
     pop1 = inflate_hist(hist1, reads)
     pop2 = inflate_hist(hist2, sample_depth)
-    d, p = ks_2samp(pop1, pop2)
+    d, p = stats.ks_2samp(pop1, pop2)
+    return 1-p
+
+
+def alt_ks_2samp(hist1, hist2, reads, sample_depth):
+    """
+    Calculate the distance between two populations in the form of histograms
+    Uses
+    """
+    pop1 = inflate_hist(hist1, reads)
+    hist2.normalize()
+    pop2 = np.random.choice(hist2._hist.keys(), sample_depth, p=hist2._hist.values())
+    d, p = stats.ks_2samp(pop1, pop2)
+    return 1-p
+
+
+def alt2_ks_2samp(hist1, hist2, reads):
+    """
+    Calculate the distance between two populations in the form of histograms
+    Uses
+    """
+    pop1 = inflate_hist(hist1, reads)
+    hist2.normalize()
+    pop2 = stats.rv_discrete(name='custm', values=(hist2._hist.keys(), hist2._hist.values()))
+    d, p = stats.kstest(pop1, pop2.cdf)
     return 1-p
 
 
@@ -113,6 +137,7 @@ def pop_dist_kl(hist1, hist2):
 
 def prob(hist_sample, hist_distribution):
     li, ri = get_lims(hist_sample, hist_distribution)
+
     def zero_case_log(input_number):
         if input_number >= 0.001:
             return log10(input_number)
@@ -133,6 +158,10 @@ def pop_dist(hist1, hist2, method='sub', reads=50, sample_depth=10000):
         return pop_dist_emd(hist1, hist2)
     if method == 'ks':
         return pop_dist_ks_2samp(hist1, hist2, reads, sample_depth)
+    if method == 'aks':
+        return alt_ks_2samp(hist1, hist2, reads, sample_depth)
+    if method == 'a2ks':
+        return alt2_ks_2samp(hist1, hist2, reads)
     if method == 'cor':
         return pop_dist_corr(hist1, hist2)
     if method == 'con':
