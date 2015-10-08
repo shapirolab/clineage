d53257053ad981c04257c4f4f289278d5364e330
ofirr <ofir.raz@weizmann.ac.il>
2015-10-08 18:33:46 +0300

    Added workflows and libraries.
    * Added WorkFlowCell, TamirWFC, TamirWF48, TamirWF384
    * Added Library, TamirLib
    * Added BarcodePlate

--- lib_prep/models.py
+++ lib_prep/models.py
@@ -7,10 +7,86 @@ from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-from genomes.models import TargetEnrichment
-from linapp.models import Protocol
-from wet_storage.models import SampleLocation
-#from sampling.models import CellContent
+from genomes.models import TargetEnrichment, DNABarcode
+from linapp.models import Protocol, RawData
+from wet_storage.models import SampleLocation, Plate
+from sampling.models import CellContent
+
+class WorkFlowCell(models.Model): # cell + barcode
+    content = models.ForeignKey(CellContent)
+    barcodes = models.ForeignKey(BarcodePair)
+    physical_locations = fields.GenericRelation('SampleLocation',
+                                             content_type_field='content_type',
+                                             object_id_field='object_id')
+
+class Library(models.Model): # prepared for sequencing
+    physical_locations = fields.GenericRelation('SampleLocation',
+                                         content_type_field='content_type',
+                                         object_id_field='object_id')
+
+class TamirLib(Library):
+    workflow384 = models.ForeignKey(TamirWF384)
+    wfcs = models.ManyToManyField(TamirWFC,through=TamirLibCells)
+    bluepippin_length = models.IntegerField()
+
+class TamirLibCells(models.Model):
+    wfc = models.ForeignKey(TamirWFC)
+    lib = models.ForeignKey(TamirLib)
+    volume = models.FloatField()
+
+class TamirWFC(WorkFlowCell):
+    workflow48 = models.ForeignKey(TamirWF48)
+    aar_well = models.CharField(max_length=3)
+    post_pcr1_conc = models.FloatField()
+    post_pcr1_location = fields.GenericRelation('SampleLocation',
+                                             content_type_field='content_type',
+                                             object_id_field='object_id')
+    pre_pcr2_conc = models.FloatField()
+    pre_pcr2_location = fields.GenericRelation('SampleLocation',
+                                             content_type_field='content_type',
+                                             object_id_field='object_id')
+    barcode_well = models.CharField(max_length=3)
+    dilution_well = models.CharField(max_length=3)
+    final_well = models.CharField(max_length=3)
+
+    class Meta:
+        unique_together = (
+            ("workflow48", "aar_well"),
+            ("workflow48", "barcode_well"),
+            ("workflow48", "dilution_well"),
+            ("workflow48", "final_well"),
+            # can't do uniqueness with the larger workflow
+        )
+### -------------------------------------------------------------------------------------
+class TamirWF48(models.Model):
+    workflow384 = models.ForeignKey(TamirWF384)
+    #TODO: enforce plate types
+    aar = models.ForeignKey(Plate)
+    barcode_plate = models.ForeignKey(Plate)
+    dilution_plate = models.ForeignKey(Plate)
+
+class TamirWF384(models.Model):
+    #TODO: enforce plate type
+    final_plate = models.ForeignKey(Plate)
+
+### -------------------------------------------------------------------------------------
+class BarcodePair(models.Model):
+    left = models.ForeignKey(DNABarcode)
+    right = models.ForeignKey(DNABarcode)
+### -------------------------------------------------------------------------------------
+class BarcodePlate(models.Model):
+    #TODO: enforce plate type
+    plate = models.ForeignKey(Plate)
+    barcodes = models.ManyToManyField(BarcodePair,through=BarcodePairBarcodePlate)
+### -------------------------------------------------------------------------------------
+class BarcodePairBarcodePlate(models.Model):
+    pair = models.ForeignKey(BarcodePair)
+    plate = models.ForeignKey(BarcodePlate)
+    well = models.CharField(max_length=3)
+
+    class Meta:
+        unique_together = (("plate", "well"),)
+
 
 
 ### -------------------------------------------------------------------------------------
