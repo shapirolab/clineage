513efe1d6f622429b5c2dd44ec2579ebec8d7d11
Amos Onn <amosonn@gmail.com>
2015-12-22 14:53:12 +0200

    Added migrations for removing temp fks.

---
+++ primers/parts/migrations/0002_remove_temp_fks.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('linapp', '0004_split_targets_etc'),
+        ('parts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='illuminareadingadaptor1cuts',
+            name='old_tail',
+        ),
+        migrations.RemoveField(
+            model_name='illuminareadingadaptor2cuts',
+            name='old_tail',
+        ),
+    ]
