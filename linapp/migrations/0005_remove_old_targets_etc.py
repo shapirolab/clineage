1e1d573d0a99364f76e5a3d0355a8474d848d95f
Amos Onn <amosonn@gmail.com>
2016-01-05 01:02:48 +0200

    Added removal of temp fks in reagents.

--- linapp/migrations/0005_remove_old_targets_etc.py
+++ linapp/migrations/0005_remove_old_targets_etc.py
@@ -7,6 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('reagents', '0002_remove_temp_fks'),
         ('planning', '0002_remove_temp_fks'),
         ('synthesis', '0002_remove_temp_fks'),
         ('parts', '0002_remove_temp_fks'),
---
+++ targeted_enrichment/reagents/migrations/0002_remove_temp_fks.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reagents', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='pcr1primerpairterbase',
+            name='old_te',
+        ),
+        migrations.RemoveField(
+            model_name='targetednotailprimerpairter',
+            name='old_te',
+        ),
+    ]
