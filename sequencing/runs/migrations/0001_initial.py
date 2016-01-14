b2245f414fb48f15e5ecf940fd149dd93ef7852c
Amos Onn <amosonn@gmail.com>
2016-01-14 12:25:29 +0200

    Fixed dep of runs.0001 to linapp.0002.

--- sequencing/runs/migrations/0001_initial.py
+++ sequencing/runs/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('linapp', '0001_initial'),
+        ('linapp', '0002_split_to_apps'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
