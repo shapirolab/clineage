9dd861ad52019215f21f1833d79e267ea2716d4a
Amos Onn <amosonn@gmail.com>
2016-02-07 19:13:13 +0200

    Added NGSKit, and read_length field to Machine.
    Describing various aspects of the run, which will be used to generate
    the samplesheet.

--- sequencing/runs/models.py
+++ sequencing/runs/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 from lib_prep.workflows.models import Library
+from primers.parts.models import IlluminaReadingAdaptor1, \
+    IlluminaReadingAdaptor2
 
 __author__ = 'ofirr'
 
 class MachineType(models.Model):
     company = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
+    read_length = models.IntegerField()
 
     def __unicode__(self):
         return self.company + '_' + self.model
@@ -21,12 +24,31 @@ class Machine(models.Model):
         return self.type.__unicode__() + '_' + self.machineid
 
 
+class NGSKit(models.Model):
+    reading_adaptor1 = models.ForeignKey(IlluminaReadingAdaptor1)
+    reading_adaptor2 = models.ForeignKey(IlluminaReadingAdaptor2)
+
+    @property
+    def adapter(self):
+        """
+        Return the value for the Adapter field in the SampleSheet.
+        """
+        return reading_adaptor2.sequence[1:].rev_comp()
+
+    @property
+    def adapterread2(self):
+        """
+        Return the value for the AdapterRead2 field in the SampleSheet.
+        """
+        return reading_adaptor1.sequence.rev_comp()
+
+
 class NGSRun(models.Model):
     libraries = models.ManyToManyField(Library)
     bcl_directory = models.FilePathField(allow_files=False, allow_folders=True, null=True)
     name = models.CharField(max_length=100, unique=True)
     machine = models.ForeignKey(Machine)
-    # TODO: add seqeuencing primers. kit?
+    kit = models.ForeignKey(NGSKit)
     user = models.ForeignKey(User)
     date = models.DateField()
 
