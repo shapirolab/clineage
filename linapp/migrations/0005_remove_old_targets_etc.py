438d74e836ce27d88185bcda5e4ed93d027efe9d
Ofir Raz <ofirr@math102-lx.wisdom.weizmann.ac.il>
2016-02-16 15:44:44 +0200

    Removed redundant RemoveField-s in linapp.0005
    Many RemoveField-s for ForeignKey-s can be retained, if we re-order the
    DeleteModel-s properly.

--- linapp/migrations/0005_remove_old_targets_etc.py
+++ linapp/migrations/0005_remove_old_targets_etc.py
@@ -15,83 +15,17 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # These are temp fields.
-        migrations.RemoveField(
-            model_name='targetenrichment',
-            name='new_te',
-        ),
-        migrations.RemoveField(
-            model_name='target',
-            name='new_target',
-        ),
-        # Up to here.
-        #migrations.RemoveField(
-            #model_name='microsatellite',
-            #name='target_ptr',
-        #),
         migrations.RemoveField(
             model_name='panel',
             name='targets',
         ),
         migrations.RemoveField(
-            model_name='primer',
-            name='sequence',
-        ),
-        migrations.RemoveField(
-            model_name='primer',
-            name='tail',
-        ),
-        #migrations.RemoveField(
-            #model_name='primer',
-            #name='target_ptr',
-        #),
-        migrations.RemoveField(
             model_name='primersmultiplex',
             name='primers',
         ),
         migrations.RemoveField(
-            model_name='restrictionsite',
-            name='restriction_type',
-        ),
-        #migrations.RemoveField(
-            #model_name='restrictionsite',
-            #name='target_ptr',
-        #),
-        #migrations.RemoveField(
-            #model_name='snp',
-            #name='target_ptr',
-        #),
-        migrations.RemoveField(
-            model_name='target',
-            name='chromosome',
-        ),
-        migrations.RemoveField(
             model_name='target',
             name='partner',
-        ),
-        migrations.RemoveField(
-            model_name='target',
-            name='referencevalue',
-        ),
-        migrations.RemoveField(
-            model_name='target',
-            name='type',
-        ),
-        migrations.RemoveField(
-            model_name='targetenrichment',
-            name='chromosome',
-        ),
-        migrations.RemoveField(
-            model_name='targetenrichment',
-            name='left',
-        ),
-        migrations.RemoveField(
-            model_name='targetenrichment',
-            name='partner',
-        ),
-        migrations.RemoveField(
-            model_name='targetenrichment',
-            name='right',
         ),
         migrations.RemoveField(
             model_name='targetenrichment',
@@ -99,15 +33,7 @@ class Migration(migrations.Migration):
         ),
         migrations.RemoveField(
             model_name='targetenrichment',
-            name='type',
-        ),
-        migrations.RemoveField(
-            model_name='targetenrichment',
-            name='validation_failure',
-        ),
-        migrations.RemoveField(
-            model_name='targetenrichmenttype',
-            name='protocol',
+            name='partner',
         ),
         migrations.DeleteModel(
             name='Microsatellite',
@@ -116,10 +42,13 @@ class Migration(migrations.Migration):
             name='Panel',
         ),
         migrations.DeleteModel(
-            name='Primer',
+            name='PrimersMultiplex',
         ),
         migrations.DeleteModel(
-            name='PrimersMultiplex',
+            name='TargetEnrichment',
+        ),
+        migrations.DeleteModel(
+            name='Primer',
         ),
         migrations.DeleteModel(
             name='PrimerTail',
@@ -131,16 +60,10 @@ class Migration(migrations.Migration):
             name='RestrictionSiteType',
         ),
         migrations.DeleteModel(
-            name='Sequence',
-        ),
-        migrations.DeleteModel(
             name='SNP',
         ),
         migrations.DeleteModel(
             name='Target',
-        ),
-        migrations.DeleteModel(
-            name='TargetEnrichment',
         ),
         migrations.DeleteModel(
             name='TargetEnrichmentFailureType',
@@ -150,5 +73,8 @@ class Migration(migrations.Migration):
         ),
         migrations.DeleteModel(
             name='TargetType',
+        ),
+        migrations.DeleteModel(
+            name='Sequence',
         ),
     ]
