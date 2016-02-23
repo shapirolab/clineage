7b157536494b6d58af83dcd393840e104b9abd2f
ofirr <ofir.raz@weizmann.ac.il>
2016-02-23 10:22:07 +0200

    Added some slice fixtures to genomes test_models

--- genomes/test_models.py
+++ genomes/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from models import Assembly, Chromosome
+from models import Assembly, Chromosome, DNASlice
 from misc.test_models import human_taxa
 
 @pytest.fixture()
@@ -21,6 +21,77 @@ def hg19_chromosome(hg19_assembly):
         cyclic=False,
     )
     return c
+
+
+@pytest.fixture()
+def slice_28727_left(hg19_chromosome):
+    dnas = DNASlice.objects.create(
+        chromosome=hg19_chromosome,
+        start_pos=81316094,
+        end_pos=81316116,
+    )
+    return dnas
+
+
+@pytest.fixture()
+def slice_28727_target_a(hg19_chromosome):
+    dnas = DNASlice.objects.create(
+        chromosome=hg19_chromosome,
+        start_pos=81316131,
+        end_pos=81316199,
+    )
+    return dnas
+
+
+@pytest.fixture()
+def slice_28727_target_b(hg19_chromosome):
+    dnas = DNASlice.objects.create(
+        chromosome=hg19_chromosome,
+        start_pos=81316201,
+        end_pos=81316236,
+    )
+    return dnas
+
+
+@pytest.fixture()
+def slice_28727_right(hg19_chromosome):
+    dnas = DNASlice.objects.create(
+        chromosome=hg19_chromosome,
+        start_pos=81316243,
+        end_pos=81316265,
+    )
+    return dnas
+
+
+@pytest.fixture()
+def slice_28734_left(hg19_chromosome):
+    dnas = DNASlice.objects.create(
+        chromosome=hg19_chromosome,
+        start_pos=54384674,
+        end_pos=54384696,
+    )
+    return dnas
+
+
+@pytest.fixture()
+def slice_28734_target_a(hg19_chromosome):
+    dnas = DNASlice.objects.create(
+        chromosome=hg19_chromosome,
+        start_pos=54384788,
+        end_pos=54384805,
+    )
+    return dnas
+
+
+@pytest.fixture()
+def slice_28734_right(hg19_chromosome):
+    dnas = DNASlice.objects.create(
+        chromosome=hg19_chromosome,
+        start_pos=54384807,
+        end_pos=54384829,
+    )
+    return dnas
+
 
 
 @pytest.mark.django_db
