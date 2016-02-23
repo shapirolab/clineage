86997df5a527fd245dd4f474eba607e409b55728
ofirr <ofir.raz@weizmann.ac.il>
2016-02-23 10:22:50 +0200

    Added tests and fixtures to targeted_enrichment/planning

---
+++ targeted_enrichment/planning/test_models.py
@@ -0,0 +1,104 @@
+import pytest
+from models import UGSPlus, UGSMinus, TargetEnrichment, Microsatellite
+
+from misc.test_models import human_taxa
+from genomes.test_models import hg19_assembly, hg19_chromosome, \
+    slice_28727_left, slice_28727_right, slice_28727_target_a, slice_28727_target_b,\
+    slice_28734_left, slice_28734_right, slice_28734_target_a
+
+
+@pytest.fixture()
+def ugs_28727_left(slice_28727_left):
+    ugsp = UGSPlus.objects.create(
+        slice=slice_28727_left,
+    )
+    return ugsp
+
+
+@pytest.fixture()
+def ugs_28727_right(slice_28727_right):
+    ugsm = UGSMinus.objects.create(
+        slice=slice_28727_right,
+    )
+    return ugsm
+
+
+@pytest.fixture()
+def ms_28727_a(slice_28727_target_a):
+    ms = Microsatellite.objects.create(
+        name='X_81316131_81316199',
+        slice=slice_28727_target_a,
+        repeat_unit_len=3,
+        repeat_unit_type='AAG',
+        repeat_number=23,
+    )
+    return ms
+
+
+@pytest.fixture()
+def ms_28727_b(slice_28727_target_b):
+    ms = Microsatellite.objects.create(
+        name='X_81316201_81316236',
+        slice=slice_28727_target_b,
+        repeat_unit_len=3,
+        repeat_unit_type='AGC',
+        repeat_number=12,
+    )
+    return ms
+
+
+@pytest.fixture()
+def te_28727(hg19_chromosome, ugs_28727_left, ugs_28727_right, ms_28727_a, ms_28727_b):
+    te = TargetEnrichment.objects.create(
+        chromosome=hg19_chromosome,
+        left=ugs_28727_left,
+        right=ugs_28727_right,
+        planning_version=1,
+    )
+    te.targets = [ms_28727_a, ms_28727_b]
+    return te
+
+
+@pytest.fixture()
+def ugs_28734_left(slice_28734_left):
+    ugsp = UGSPlus.objects.create(
+        slice=slice_28734_left,
+    )
+    return ugsp
+
+
+@pytest.fixture()
+def ugs_28734_right(slice_28734_right):
+    ugsm = UGSMinus.objects.create(
+        slice=slice_28734_right,
+    )
+    return ugsm
+
+
+@pytest.fixture()
+def ms_28734_a(slice_28734_target_a):
+    ms = Microsatellite.objects.create(
+        name='X_54384788_54384805',
+        slice=slice_28734_target_a,
+        repeat_unit_len=3,
+        repeat_unit_type='AAG',
+        repeat_number=6,
+    )
+    return ms
+
+
+@pytest.fixture()
+def te_28734(hg19_chromosome, ugs_28734_left, ugs_28734_right, ms_28734_a):
+    te = TargetEnrichment.objects.create(
+        chromosome=hg19_chromosome,
+        left=ugs_28734_left,
+        right=ugs_28734_right,
+        planning_version=1,
+    )
+    te.targets = [ms_28734_a]
+    return te
+
+
+@pytest.mark.django_db
+def test_te(te_28727):
+    assert te_28727.targets.get(name='X_81316201_81316236').slice.start_pos == 81316201
