1d7d20e98bd2b1038c5f2f0378b0a2349896166e
ofirr <ofir.raz@weizmann.ac.il>
2016-02-24 13:07:39 +0200

    Added bowtie2 wrapping to sequencing/analysis/index_reads.py

--- sequencing/analysis/index_reads.py
+++ sequencing/analysis/index_reads.py
@@ -2,3 +2,29 @@ from plumbum import local
 
 BOWTIE2BUILD_PATH = "/net/mraid11/export/data/dcsoft/home/Adam/Software/bowtie2-2.2.2/bowtie2-build"
 bowtie2build = local[BOWTIE2BUILD_PATH]
+
+# bowtie2-build \
+#     ${fastq_folder}/Cluster/processing/${fastqName}/merged/${fastq_R1}_final_merged.fa \
+#     ${fastq_folder}/Cluster/processing/${fastqName}/merged/${fastq_R1}_final_merged
+
+
+
+BOWTIE2_PATH = "/net/mraid11/export/data/dcsoft/home/Adam/Software/bowtie2-2.2.2/bowtie2"
+bowtie2 = local[BOWTIE2_PATH]
+bowtie2_with_defaults = bowtie2["-p", "24",
+                                "-a",
+                                "--very-sensitive",]
+
+# bowtie2 \
+#     -p 24 \
+#     -a \
+#     --very-sensitive \
+#     -x ${fastq_folder}/Cluster/processing/${fastqName}/merged/${fastq_R1}_final_merged \
+#     -f ${TargetsFolder}/${TargetsName}_Primers.fa \
+#         > ${fastq_folder}/Cluster/processing/${fastqName}/merged/${fastq_R1}_final_merged_Primers.sam
+
+# /net/mraid11/export/data/dcsoft/home/Adam/Software/bowtie2-2.2.2/bowtie2 \
+#     -a --very-sensitive \
+#     -x index_1 \
+#     -f /net/mraid11/export/dcstor/Ofir/ngs_fixtures/28727_and_28734_Primers.fa \
+#     -S test2.sam
