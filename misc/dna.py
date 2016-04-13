f8ef34fa06f27b6fee30fb2eccbfd381ab5a828b
ofirr <ofir.raz@weizmann.ac.il>
2016-04-13 16:28:30 +0300

    Fixed dna to python3.
    (cherry picked from commit fcfd4ad57cba869bc993088ae8772a57b93c8db7)
    (cherry picked from commit ed46ba7bfa6cc7a036fbd0adad73c5b9495f516e)

--- misc/dna.py
+++ misc/dna.py
@@ -1,28 +1,34 @@
 
-basecomp = {'A': 'T',
-            'T': 'A',
-            'C': 'G',
-            'G': 'C'}
+basecomp = {b'A': b'T',
+            b'T': b'A',
+            b'C': b'G',
+            b'G': b'C'}
 
 def _rc(seq):
     n = len(seq)
     l = [None]*n
-    for i in xrange(n):
-        l[n-i-1] = basecomp[seq[i]]
-    return ''.join(l)
+    for i in range(n):
+        l[n-i-1] = basecomp[seq[i:i+1]]
+    return b''.join(l)
 
 class DNA(object):
 
-    def __init__(self,seq):
-        # FIXME: this should change when we fix the db to give us bytearray.
-        if isinstance(seq,str):
+    def __init__(self, seq):
+        # FIXME: this should change when we fix the db to give us bytes.
+        if isinstance(seq, str):
             s = seq.upper()
             for i in s:
                 if i not in "ACGT":
                     raise ValueError("Invalid DNA base - not one of A,C,T,G.")
+            self._seq = s.encode("ascii")
+        elif isinstance(seq, bytes):
+            s = seq.upper()
+            for i in s:
+                if i not in b"ACGT":
+                    raise ValueError("Invalid DNA base - not one of A,C,T,G.")
             self._seq = s
         else:
-            raise TypeError("DNA accepts only string (bytearray) objects.")
+            raise TypeError("DNA accepts only string (bytes) objects.")
 
     @property
     def seq(self):
@@ -38,24 +44,24 @@ class DNA(object):
         # Can be optimized to omit ctor check.
         return DNA(self.seq + other.seq)
 
-    def __getitem__(self,num_or_slice):
-        if not isinstance(num_or_slice,slice):
+    def __getitem__(self, num_or_slice):
+        if not isinstance(num_or_slice, slice):
             raise ValueError("Only returns slices")
         if num_or_slice.step is not None:
             raise ValueError("No step")
         return DNA(self.seq[num_or_slice])
 
-    def __str__(self):
+    def __bytes__(self):
         return self.seq
 
-    def __unicode__(self):
+    def __str__(self):
         return self.seq.decode("ascii")
 
     def __repr__(self):
         return "DNA(\"%s\")" % (self.seq)
 
     def __eq__(self, other):
-        if not isinstance(other,DNA):
+        if not isinstance(other, DNA):
             raise TypeError("unsupported operand type(s) for ==: '%s' and '%s'"
                             % (type(self), type(other)))
         return self.seq == other.seq
