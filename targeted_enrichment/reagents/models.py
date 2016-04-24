9113c8720463226609aeda82490316f099e194d5
ofirr <ofir.raz@weizmann.ac.il>
2016-04-24 12:07:04 +0300

    Added null=True to amplicon fields in targeted_enrichment.reagents.models
    Added automatic migrations
    Added custom migration for populating Microsatellite.repeat_unit_ref_seq
    Added MS related utils

--- targeted_enrichment/reagents/models.py
+++ targeted_enrichment/reagents/models.py
@@ -54,19 +54,19 @@ class PCR1PrimerPairTERBase(TwoPrimersUnicodeMixin, TargetedEnrichmentReagent):
 
 
 class PCR1PrimerPairTER(PCR1PrimerPairTERBase):
-    amplicon = models.ForeignKey(PlainTargetedAmplicon)
+    amplicon = models.ForeignKey(PlainTargetedAmplicon, null=True)
     left_primer = models.ForeignKey(PCR1PlusPrimer)
     right_primer = models.ForeignKey(PCR1MinusPrimer)
 
 
 class PCR1WithCompanyTagPrimerPairTER(PCR1PrimerPairTERBase):
-    amplicon = models.ForeignKey(TargetedAmpliconWithCompanyTag)
+    amplicon = models.ForeignKey(TargetedAmpliconWithCompanyTag, null=True)
     left_primer = models.ForeignKey(PCR1WithCompanyTagPlusPrimer)
     right_primer = models.ForeignKey(PCR1WithCompanyTagMinusPrimer)
 
 
 class PCR1PrimerPairTERDeprecated(PCR1PrimerPairTERBase): #TODO: kill?
-    amplicon = models.ForeignKey(PlainTargetedAmplicon)
+    amplicon = models.ForeignKey(PlainTargetedAmplicon, null=True)
     left_primer = models.ForeignKey(PCR1PlusPrimer)
     right_primer = models.ForeignKey(PCR1MinusPrimer)
 
@@ -77,7 +77,7 @@ class TargetedNoTailPrimerPairTER(TargetedEnrichmentReagent, TwoPrimersUnicodeMi
 
 
 class ShortPadlockFirstTER(TargetedEnrichmentReagent):
-    amplicon = models.ForeignKey(UMITargetedAmplicon)
+    amplicon = models.ForeignKey(UMITargetedAmplicon, null=True)
     padlock = models.ForeignKey(ShortPadlockFirst)
 
     def __unicode__(self):
