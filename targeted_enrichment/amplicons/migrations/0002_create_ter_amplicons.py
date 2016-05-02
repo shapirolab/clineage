ffad13edebc63fb4783c135329af6b25dbf15036
Amos Onn <amosonn@gmail.com>
2016-05-02 21:09:57 +0300

    Implemented 0002_create_ter_amplicons.  FIXME on UMIs.

--- targeted_enrichment/amplicons/migrations/0002_create_ter_amplicons.py
+++ targeted_enrichment/amplicons/migrations/0002_create_ter_amplicons.py
@@ -6,8 +6,107 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+TER_SELECT_RELATED = [
+    "left_primer",
+    "left_primer__ugs",
+    "left_primer__ugs__slice",
+    "right_primer",
+    "right_primer__ugs",
+    "right_primer__ugs__slice",
+]
+
+
+def get_target_amplicon_dict(ter):
+    if ter.left_primer.ugs.slice.chromosome_id != \
+        ter.right_primer.ugs.slice.chromosome_id:
+        raise IntegrityError(
+            "TER {} has ugss with slices from different " \
+            "chromosomes.".format(ter))
+    slice = DNASlice.objects.using(db_alias).create(
+        chromosome_id=ter.left_primer.ugs.slice.chromosome_id,
+        start_pos=ter.left_primer.ugs.slice.end_pos + 1,  # 1 based!
+        end_pos=ter.right_primer.ugs.slice.start_pos - 1,  # 1 based!
+    )
+    return {
+        "left_ugs": ter.left_primer.ugs,
+        "right_ugs": ter.left_primer.ugs,
+        "slice": slice,
+    }
+
+
+def populate_plain_targeted_ters(apps, schema_editor):
+    db_alias = schema_editor.connection.alias
+    PCR1PrimerPairTER = apps.get_model('reagents', 'PCR1PrimerPairTER')
+    PCR1PrimerPairTERDeprecated = apps.get_model('reagents', 'PCR1PrimerPairTERDeprecated')
+    TargetedNoTailPrimerPairTER = apps.get_model('reagents', 'TargetedNoTailPrimerPairTER')
+    PlainTargetedAmplicon = apps.get_model('amplicons', 'PlainTargetedAmplicon')
+    DNASlice = apps.get_model('genomes', 'DNASlice')
+    for TER in [
+        PCR1PrimerPairTER,
+        PCR1PrimerPairTERDeprecated,
+        TargetedNoTailPrimerPairTER,
+    ]:
+        for ter in TER.objects.using(db_alias).select_related(
+            *TER_SELECT_RELATED):
+            amplicon = PlainTargetedAmplicon.objects.using(db_alias).create(
+                **get_target_amplicon_dict(ter)
+            )
+            ter.amplicon = amplicon
+            ter.save()
+
+
+def populate_pcr1withcompanytagprimerpairter(apps, schema_editor):
+    db_alias = schema_editor.connection.alias
+    PCR1WithCompanyTagPrimerPairTER = apps.get_model('reagents', 'PCR1WithCompanyTagPrimerPairTER')
+    TargetedAmpliconWithCompanyTag = apps.get_model('amplicons', 'TargetedAmpliconWithCompanyTag')
+    for ter in TargetedAmpliconWithCompanyTag.objects.using(db_alias). \
+        select_related(*TER_SELECT_RELATED):
+        amplicon = TargetedAmpliconWithCompanyTag.objects.using(db_alias). \
+            create(
+            left_tag=ter.left_primer.tag,
+            right_tag=DNA(ter.right_primer.tag).rev_comp().seq.decode("ascii"),
+            **get_target_amplicon_dict(ter)
+        )
+        ter.amplicon = amplicon
+        ter.save()
+
+
+def populate_shortpadlockfirstter(apps, schema_editor):
+    db_alias = schema_editor.connection.alias
+    ShortPadlockFirstTER = apps.get_model('reagents', 'ShortPadlockFirstTER')
+    UMITargetedAmplicon = apps.get_model('amplicons', 'UMITargetedAmplicon')
+    for ter in ShortPadlockFirstTER.objects.using(db_alias). \
+        select_related(
+            "padlock",
+            "padlock__left_ugs",
+            "padlock__left_ugs__slice",
+            "padlock__right_ugs",
+            "padlock__right_ugs__slice",
+        ):
+        if ter.padlock.left_ugs.slice.chromosome_id != \
+            ter.padlock.right_ugs.slice.chromosome_id:
+            raise IntegrityError(
+                "TER {} has ugss with slices from different " \
+                "chromosomes.".format(ter))
+        slice = DNASlice.objects.using(db_alias).create(
+            chromosome_id=ter.padlock.left_ugs.slice.chromosome_id,
+            start_pos=ter.padlock.left_ugs.slice.end_pos + 1,  # 1 based!
+            end_pos=ter.padlock.right_ugs.slice.start_pos - 1,  # 1 based!
+        )
+        amplicon = UMITargetedAmplicon.objects.using(db_alias).create(
+            umi_length=3,  # FIXME!
+            left_ugs=ter.padlock.left_ugs,
+            right_ugs=ter.padlock.left_ugs,
+            slice=slice,
+        )
+        ter.amplicon = amplicon
+        ter.save()
+
+
 def populate_ter_amplicons(apps, schema_editor):
-    pass
+    populate_plain_targeted_ters(apps, schema_editor)
+    populate_pcr1withcompanytagprimerpairter(apps, schema_editor)
+    populate_shortpadlockfirstter(apps, schema_editor)
 
 
 class Migration(migrations.Migration):
