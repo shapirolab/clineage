7d8a8973ef03348c852a47906be58eff781e054d
Amos Onn <amosonn@gmail.com>
2016-05-08 18:46:30 +0300

    Implemented real test_run_parallel.

--- tests/sequencing/analysis/test_cluster.py
+++ tests/sequencing/analysis/test_cluster.py
@@ -11,6 +11,10 @@ from tests.sequencing.analysis.reads_dict import ASSEMBLED, UNASSEMBLED
 from distributed.utils_test import cluster
 from distributed.executor import Future, as_completed, Executor
 
+from sequencing.analysis.models import AdamMergedReads, AdamReadsIndex, \
+    AdamMarginAssignment, AdamAmpliconReads, AdamHistogram, AdamMSVariations, \
+    HistogramEntryReads
+
 
 @pytest.yield_fixture(scope="session")
 def executor(): 
@@ -90,5 +94,60 @@ def test_map_runmerge(executor, adam_reads_fd, sample_reads_d):
 
 @pytest.mark.django_db(transaction=True)
 def test_run_parallel(executor, demultiplexing, sample_reads_d, adam_reads_fd, requires_amplicons, requires_pmss):
-    hers = list(run_parallel(executor, demultiplexing))
-    assert hers != []
+    herss = {inc: set(run_parallel(executor, demultiplexing, inc)) for \
+        inc in ["M", "F"]}
+    for inc, hers in herss.items():
+        parts = set()
+        for her in hers:
+            amp = her.amplicon_id
+            sr = her.histogram.subclass.amplicon_reads.margin_assignment.reads_index.merged_reads.sample_reads
+            bc = sr.barcoded_content_id
+            l_id = sr.library_id
+            gen = frozenset((msg.microsatellite_id, msg.repeat_number) for \
+                msg in her.microsatellite_genotypes.all())
+            parts.add((l_id, bc, amp, gen))
+            
+            her_fnames_d = {
+                R1: her.fastq1,
+                R2: her.fastq2,
+                RM: her.fastqm,
+            }
+            
+            if inc == "M":
+                ref_reads_d = adam_reads_fd[l_id, bc, ASSEMBLED, amp, gen]
+            else:  # inc == "F"
+                ref_reads_d = {
+                    R1: adam_reads_fd[l_id, bc, ASSEMBLED, amp, gen][R1] + \
+                        adam_reads_fd[l_id, bc, UNASSEMBLED, amp, gen][R1],
+                    R2: adam_reads_fd[l_id, bc, ASSEMBLED, amp, gen][R2] + \
+                        adam_reads_fd[l_id, bc, UNASSEMBLED, amp, gen][R2],
+                    RM: adam_reads_fd[l_id, bc, ASSEMBLED, amp, gen][RM] + \
+                        adam_reads_fd[l_id, bc, UNASSEMBLED, amp, gen][R1],
+                }
+            
+            for r in [R1, R2, RM]:
+                assert set(srs_to_tups(  # TODO: get informative error on genotyping mismatch
+                    SeqIO.parse(her_fnames_d[r], "fastq"))
+                ) == \
+                set(srs_to_tups(
+                    ref_reads_d[r]
+                ))
+            assert her.num_reads == \
+                len(ref_reads_d[RM])
+        ref_parts = set()
+        for key_tups in adam_reads_fd:
+            if len(key_tups) < 5:
+                continue
+            l_id2, bc2, t2, amp2, gen2 = key_tups
+            if t2 == ASSEMBLED or inc == "F":
+                ref_parts.add((l_id2, bc2, amp2, gen2))
+        assert ref_parts == parts
+    for Model in [
+        AdamHistogram,
+        AdamMSVariations,  # FIXME: get these from outside.
+        AdamAmpliconReads,
+        AdamMarginAssignment,
+        AdamReadsIndex,
+        AdamMergedReads,
+    ]:
+        Model.objects.all().delete()
