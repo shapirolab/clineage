3b71641c63730a423a3d66eff8db22a2caf8c0c2
ofirr <ofir.raz@weizmann.ac.il>
2016-05-15 15:16:36 +0300

    Added missing DNA import

--- targeted_enrichment/amplicons/migrations/0002_create_ter_amplicons.py
+++ targeted_enrichment/amplicons/migrations/0002_create_ter_amplicons.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import django.db.models.deletion
-
+from misc.dna import DNA
 
 TER_SELECT_RELATED = [
     "left_primer",
