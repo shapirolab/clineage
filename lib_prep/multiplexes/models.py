afaa700bd3a780ead7dbe7968b5961de62f864c2
Amos Onn <amosonn@gmail.com>
2016-05-17 16:47:30 +0300

    Added Multiplexes and Panels for OM6.

--- lib_prep/multiplexes/models.py
+++ lib_prep/multiplexes/models.py
@@ -1,7 +1,8 @@
 from django.contrib.contenttypes import fields
 from django.db import models
 from targeted_enrichment.planning.models import TargetEnrichment
-from targeted_enrichment.reagents.models import PCR1PrimerPairTERBase
+from targeted_enrichment.reagents.models import PCR1PrimerPairTERBase, \
+    OM6PadlockTER
 from wet_storage.models import SampleLocation
 
 ### -------------------------------------------------------------------------------------
@@ -26,6 +27,17 @@ class TERMultiplex(models.Model): # TODO: move to primers, m2m to TER.
         return self.name
 
 
+class Panel(models.Model):  # PCR1MultiplexCollection
+    name = models.CharField(max_length=20)
+    # mpxs = models.ManyToManyField(TERMultiplex)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
 class PCR1Multiplex(TERMultiplex):
     ters = models.ManyToManyField(PCR1PrimerPairTERBase)
     #physical_locations = fields.GenericRelation(SampleLocation,
@@ -34,8 +46,17 @@ class PCR1Multiplex(TERMultiplex):
     #TODO: physical_locations(MPXPlate)
 
 
-class PCR1Panel(models.Model):  # PCR1MultiplexCollection
-    name = models.CharField(max_length=20)
+class PCR1Panel(Panel):  # PCR1MultiplexCollection
     mpxs = models.ManyToManyField(PCR1Multiplex)
-    def __str__(self):
-        return self.name
+
+
+class OM6Oligomix(TERMultiplex):
+    ters = models.ManyToManyField(OM6PadlockTER)
+    #physical_locations = fields.GenericRelation(SampleLocation,
+                               #content_type_field='content_type',
+                               #object_id_field='object_id')
+    #TODO: physical_locations(MPXPlate)
+
+
+class OM6Panel(Panel):
+    mixs = models.ManyToManyField(OM6Oligomix)
