57c5091d47b8cf321ad91e8516de6e67ceb20f89
Amos Onn <amosonn@gmail.com>
2016-05-22 18:12:37 +0300

    Fixed 2to3 regression in wet_storage and sampling migs.

--- sampling/migrations/0001_initial.py
+++ sampling/migrations/0001_initial.py
@@ -70,7 +70,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
-                ('sex', models.CharField(max_length=1, choices=[(b'M', b'Male'), (b'F', b'Female')])),
+                ('sex', models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])),
                 ('born', models.DateTimeField(null=True, blank=True)),
                 ('comment', models.TextField(null=True, blank=True)),
                 ('sacrificed', models.DateTimeField(null=True, blank=True)),
