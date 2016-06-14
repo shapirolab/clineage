0513da8bb8ee88a22408f015dfc921d1c3204f1e
ofirr <ofir.raz@weizmann.ac.il>
2016-06-14 12:06:14 +0300

    Added TODO for setting RLIMIT_CORE

--- sequencing/analysis/adamiya.py
+++ sequencing/analysis/adamiya.py
@@ -382,8 +382,10 @@ def list_iterator(f):
 
 
 def run_parallel(executor, sample_reads, included_reads="F", mss_version=0, read_padding=5, ref_padding=50):
-    merged_reads = executor.map(close_connection_and(merge), sample_reads, pure=False)
-    reads_indices = executor.map(close_connection_and(create_reads_index), merged_reads,
+    # TODO: set resource.getrlimit(resource.RLIMIT_CORE) to something low for all bowtie2 related jobs
+    # *currently in dworker.q
+    merged_reads = executor.map(merge, sample_reads, pure=False)
+    reads_indices = executor.map(create_reads_index, merged_reads,
         itertools.repeat(included_reads), itertools.repeat(read_padding), pure=False)
     adam_margin_assignments = executor.map(close_connection_and(align_primers_to_reads),
         reads_indices, pure=False)
