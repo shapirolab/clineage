5799697453486f54a70c6790c88a8c7568c7e11f
Amos Onn <amosonn@gmail.com>
2016-07-21 19:51:41 +0300

    First version of xlsx view.

--- sequencing/analysis/views.py
+++ sequencing/analysis/views.py
@@ -1,5 +1,73 @@
 
+from io import BytesIO
+import xlsxwriter
+from Bio import SeqIO
+
 from django.http import FileResponse, HttpResponseNotFound
 
+from sequencing.runs.models import NGSRun, Demultiplexing
+from sequencing.analysis.models import SampleReads, Histogram, \
+    HistogramEntryReads
+
 def summary_table(request, ngs_run):
-    return HttpResponseNotFound(b"Sorry, run does not exist.")
+    try:
+        nr = NGSRun.objects.get(name=ngs_run)
+    except NGSRun.DoesNotExist:
+        return HttpResponseNotFound(b"Sorry, run does not exist.")
+    try:
+        # FIXME
+        demux = Demultiplexing.objects.get(ngs_run__name=ngs_run)
+    except Demultiplexing.DoesNotExist:
+        return HttpResponseNotFound(b"Sorry, run not analysed.")
+    bio = BytesIO()
+    workbook = xlsxwriter.Workbook(bio)
+    for library in nr.libraries.select_subclasses():
+        bcs = list(library.barcoded_contents)
+        # rev_bcs = {bc: i+1 for i in enumerate(bcs)}
+        srs = list(SampleReads.objects.filter(
+            demux=demux,
+            library=library,
+            barcoded_content__in=bcs,
+        ))
+        assert len(srs) == len(bcs)
+        rev_sr_ids = {sr.id: i+1 for i in enumerate(srs)}
+        amplicons = list(library.amplicons)
+        rev_amp_ids = {amp.id: i+1 for i in enumerate(amplicons)}
+        SUMMARY_ROWS = 1 + len(amplicons)
+
+        w = workbook.add_worksheet(name=library.name)
+
+        w.write(0, 0, "Locus")
+        w.write(SUMMARY_ROWS, 0, "TotalReads")
+        for sr in srs:
+            col = rev_sr_ids[sr.id]
+            w.write(0, col, sr.barcoded_content.cell.name)
+            # FIXME
+            with open(sr.fastq1) as f:
+                seq = SeqIO.parse(f, format="fastq")
+                n = sum(1 for x in seq)
+            w.write(SUMMARY_ROWS, col, n)
+        for amp_id, row in rev_amps.items():
+            # TODO: is this the id we want? teid? ter-id?
+            w.write(row, 0, amp_id)
+
+        for h in Histogram.objects.filter(
+            # FIXME: microsatellites_version
+            amplicon__in=amplicons,
+            sample_reads__in=srs,
+        ):
+            n = HistogramEntryReads.objects.filter(histogram=histogram) \
+                .aggregate(Sum('num_reads')).popitem()[1]
+            w.write(
+                rev_amp_ids[h.amplicon_id],
+                rev_sr_ids[h.sample_reads_id],
+                n,
+            )
+    workbook.close()
+    bio.seek(0)
+    response = FileResponse(bio,
+        content_type="application/vnd.openxmlformats-officedocument" \
+            ".spreadsheetml.sheet"
+        )
+    # response["Content-Disposition"] = 'attachment; filename="%s"' % (fname,)
+    return response
