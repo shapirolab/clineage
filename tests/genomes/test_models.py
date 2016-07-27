b900ddf9b47ad2fb8d9f0ab60858b6a3325405b9
Amos Onn <amosonn@gmail.com>
2016-07-27 17:50:04 +0300

    Added test for DNASliceIntersection.

--- tests/genomes/test_models.py
+++ tests/genomes/test_models.py
@@ -14,3 +14,14 @@ def test_chromosome(hg19_chromosome):
 @pytest.mark.django_db
 def test_dnaslice(slice_28727_left):
     assert str(slice_28727_left) == "X:81316094-81316116"
+
+
+@pytest.mark.skipif(pytest.config.getoption("nomigrations"), reason="No migrations, no view.")
+@pytest.mark.django_db
+def test_dnasliceintersection(slice_28727_target_a, slice_28727_target_b, slice_28727_amplicon):
+    assert set(slice_28727_amplicon.contains.all()) == set([slice_28727_target_a, slice_28727_target_b])
+    assert set(slice_28727_amplicon.contained.all()) == set()
+    assert set(slice_28727_target_a.contains.all()) == set()
+    assert set(slice_28727_target_a.contained.all()) == set([slice_28727_amplicon])
+    assert set(slice_28727_target_b.contains.all()) == set()
+    assert set(slice_28727_target_b.contained.all()) == set([slice_28727_amplicon])
