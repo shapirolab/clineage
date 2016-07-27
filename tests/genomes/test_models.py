bc50d2a9132c827b0d4541de141ea0b2e5adcb88
Amos Onn <amosonn@gmail.com>
2016-07-27 17:49:27 +0300

    Improved tests for genomes.models.

--- tests/genomes/test_models.py
+++ tests/genomes/test_models.py
@@ -3,9 +3,14 @@ import pytest
 
 @pytest.mark.django_db
 def test_assembly(hg19_assembly):
-    assert hg19_assembly.friendly_name == "hg19"
+    assert str(hg19_assembly) == "hg19"
 
 
 @pytest.mark.django_db
 def test_chromosome(hg19_chromosome):
-    assert hg19_chromosome.name == "X"
+    assert str(hg19_chromosome) == "hg19:X"
+
+
+@pytest.mark.django_db
+def test_dnaslice(slice_28727_left):
+    assert str(slice_28727_left) == "X:81316094-81316116"
