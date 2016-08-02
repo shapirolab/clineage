e121842f219c9029f6ec9bd44d9f07932bf2a005
Amos Onn <amosonn@gmail.com>
2016-08-02 16:49:52 +0300

    Added loggedin_client fixture to accounts.conftest.
    Added hashed password to user, and a fixture which logs the client in.

--- tests/accounts/conftest.py
+++ tests/accounts/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth.hashers import make_password
 
 
 @pytest.fixture()
@@ -6,7 +7,14 @@ def user(db,django_user_model):
     User = django_user_model
     u = User.objects.create(
         username="Shlomo",
+        password=make_password("abcd"),
     )
     # So our objects don't have "special" objects in fields
     u = User.objects.get(pk=u.pk)
     return u
+
+@pytest.fixture()
+def loggedin_client(client):
+    resp = client.post('/accounts/login/', {'username': 'Shlomo', 'password': 'abcd'})
+    assert resp.status_code in [200, 302]
+    return client
