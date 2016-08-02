d603356dede7857b8bbe00dc0e524b2ebf50b2ed
Amos Onn <amosonn@gmail.com>
2016-08-02 16:50:28 +0300

    Added preliminary test for analysis.views.

---
+++ tests/sequencing/analysis/test_views.py
@@ -0,0 +1,20 @@
+
+import pytest
+
+import io
+import openpyxl
+
+def test_summary_table(adam_histogram_entry_reads_d, loggedin_client):
+    resp = loggedin_client.get("/analysis/TestRun/summary.xlsx")
+    assert resp.status_code == 200
+    assert resp["Content-Disposition"] == \
+        'attachment; filename="TestRun_mapped_reads.xlsx"'
+    bio = io.BytesIO()
+    for s in resp.streaming_content:
+        bio.write(s)
+    bio.seek(0)
+    # NOTE: filename can also be file-like.
+    wb = openpyxl.load_workbook(filename=bio)
+    assert wb.active["A1"].value == "Locus"
+    # TODO: write the test.
+    # for (l_id, bc, inc, amp, msgs), her in adam_histogram_entry_reads_d.items():
