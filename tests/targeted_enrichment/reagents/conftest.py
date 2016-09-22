5792868b46a709b5a9cfb346294f18bf30071119
ofirr <ofir.raz@weizmann.ac.il>
2016-09-22 14:39:44 +0300

    Added TER objects to conftest

--- tests/targeted_enrichment/reagents/conftest.py
+++ tests/targeted_enrichment/reagents/conftest.py
@@ -34,3 +34,30 @@ def ter_28734(te_28734, primer_28734_left, primer_28734_right, pu_28734):
     ter = PCR1PrimerPairTER.objects.get(pk=ter.pk)
     return ter
 
+
+@pytest.fixture()
+def ter_adj_ms_1(te_adj_ms_1, primer_adj_ms_1_left, primer_adj_ms_1_right, pu_adj_ms_1):
+    ter = PCR1PrimerPairTER.objects.create(
+        te=te_adj_ms_1,
+        passed_validation=True,
+        left_primer=primer_adj_ms_1_left,
+        right_primer=primer_adj_ms_1_right,
+        amplicon=pu_adj_ms_1,
+    )
+    # So our objects don't have "special" objects in fields
+    ter = PCR1PrimerPairTER.objects.get(pk=ter.pk)
+    return ter
+
+
+@pytest.fixture()
+def ter_adj_ms_2(te_adj_ms_2, primer_adj_ms_2_left, primer_adj_ms_2_right, pu_adj_ms_2):
+    ter = PCR1PrimerPairTER.objects.create(
+        te=te_adj_ms_2,
+        passed_validation=True,
+        left_primer=primer_adj_ms_2_left,
+        right_primer=primer_adj_ms_2_right,
+        amplicon=pu_adj_ms_2,
+    )
+    # So our objects don't have "special" objects in fields
+    ter = PCR1PrimerPairTER.objects.get(pk=ter.pk)
+    return ter
