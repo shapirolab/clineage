093206cc407017d05f70db4ad5f818fdbc5d8f10
ofirr <ofir.raz@weizmann.ac.il>
2017-07-18 14:04:14 +0300

    Added allele splitting functions

--- sequencing/calling/simcor/calling.py
+++ sequencing/calling/simcor/calling.py
@@ -1,10 +1,16 @@
-from sequencing.calling.simcor.models_common import ProportionalMicrosatelliteAlleleSet, ProportionStepModelMixin, \
-    Proportions
+from sequencing.calling.simcor.models_common import ProportionalMicrosatelliteAlleleSet, ProportionStepModelMixin
 from sequencing.calling.hist import Histogram
-from sequencing.analysis.models import HistogramEntryReads
 import sys
 from sequencing.calling.models import MicrosatelliteAlleleSet
 from misc.utils import get_get_or_create
+import functools
+from collections import Counter
+from sequencing.analysis.models import HistogramEntryReads, Histogram
+from targeted_enrichment.amplicons.models import Amplicon
+from itertools import tee
+from sequencing.calling.models import CalledAlleles
+from sequencing.calling.hist import Histogram as dHistogram
+from sequencing.calling.simcor.hist_analysis import get_far_apart_highest_peaks
 
 
 def get_ms_hist(dbhist, microsatellite):
@@ -16,7 +22,7 @@ def get_ms_hist(dbhist, microsatellite):
                     hist_dict[msg.repeat_number] += her.num_reads
                 else:
                     hist_dict[msg.repeat_number] = her.num_reads
-    return Histogram(hist_dict)
+    return dHistogram(hist_dict)
 
 
 def get_closest(real_hist, sim_space, distance_function):
@@ -56,8 +62,115 @@ def call_microsatellite_histogram(calling_schema, dbhist, microsatellite):
             confidence=min_dist,
             cycle=closest_sim_hist.simulation_cycle,
         )
-    return get_get_or_create(inner, calling_schema.called_allele_class,
-         histogram=dbhist,
-         microsatellite=microsatellite,
-         calling_scheme=calling_schema,
-    )
+    return get_get_or_create(inner,
+                             calling_schema.called_allele_class,
+                             histogram=dbhist,
+                             microsatellite=microsatellite,
+                             calling_scheme=calling_schema,
+                             )
+
+
+def get_amplicon_by_ms(ms):
+    return Amplicon.objects.filter(slice__contains=ms.slice)
+
+
+def get_amplicons_by_sr(sr):
+    return set(amp.id for amp in sr.library.subclass.amplicons)
+
+
+def get_ms_amplicon(ms, sr):
+    amplicons = get_amplicon_by_ms(ms)
+    sr_amps = get_amplicons_by_sr(sr)
+    actual_amp = set(amp.id for amp in amplicons) & sr_amps
+    ampid = actual_amp.pop()
+    return Amplicon.objects.select_subclasses().get(id=ampid)
+
+
+def ms_genotypes_population_query_with_amplicon(ms, amplicon, srs, schema, confidence=0.01):
+    for sr in srs:
+        for h in Histogram.objects.filter(amplicon=amplicon, sample_reads=sr):
+            try:
+                ca = CalledAlleles.objects.select_subclasses().get(calling_scheme=schema, histogram=h,
+                                                                   microsatellite=ms)
+            except CalledAlleles.DoesNotExist:
+                continue  # No calling attempt
+            if ca.confidence > confidence:
+                continue
+            yield ca
+
+
+def ms_genotypes_population_query_with_amplicon_all(ms, amplicon, srs, schema, confidence=0.01):
+    for h in Histogram.objects.filter(amplicon=amplicon, sample_reads__in=srs):
+        try:
+            ca = CalledAlleles.objects.select_subclasses().get(calling_scheme=schema, histogram=h, microsatellite=ms)
+        except CalledAlleles.DoesNotExist:
+            continue  # No calling attempt
+        if ca.confidence > confidence:
+            continue
+        yield ca
+
+
+def ms_genotypes_population_query(ms, srs, schema, confidence=0.01):
+    amp = get_ms_amplicon(ms, list(srs)[0])  # TODO: consider iterating over all srs and asserting they all have the same panel
+    yield from ms_genotypes_population_query_with_amplicon_all(ms, amp, srs, schema, confidence=confidence)
+
+
+def get_population_kernels(genotypes, allele_number=2, minimal_distance_between_peaks=3):
+    h = dHistogram(Counter([a for ca in genotypes for a in ca.genotypes.alleles]))
+    return get_far_apart_highest_peaks(
+        h,
+        allele_number=allele_number,
+        minimal_distance_between_peaks=minimal_distance_between_peaks,
+        min_prop=0.2)
+
+
+def pairwise_overlap(iterable):
+    """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def pairwise_not_overlap(iterable):
+    """s -> (s0, s1), (s2, s3), (s4, s5), ..."""
+    a = iter(iterable)
+    return zip(a, a)
+
+
+def get_indices(peaks, max_distance_from_peak):
+    """This method gets the peaks ands returns ranges of proper values near them"""
+    p1 = peaks[0]
+    yield max(0, p1 - max_distance_from_peak)
+    for tup in pairwise_overlap(peaks):
+        p1, p2 = tup
+        p1_max = min(p1 + max_distance_from_peak, p1 + (p2 - p1) // 2)
+        p2_min = max(p1, p2 - max_distance_from_peak, p1 + (p2 - p1) // 2)
+        yield p1_max
+        yield p2_min
+    yield p2 + max_distance_from_peak
+
+
+def get_peaks_ranges(peaks, max_distance_from_peak):
+    for t in pairwise_not_overlap(get_indices(peaks, max_distance_from_peak)):
+        yield range(*t)
+
+
+def split_genotypes(ms, srs, schema, max_distance_from_peak=2):
+    cas = list(ms_genotypes_population_query(ms, srs, schema))
+    peaks = get_population_kernels(cas)
+    if len(peaks) == 1:
+        return
+    peaks.sort()
+    peaks_by_range = {p: prange for p, prange in zip(peaks, get_peaks_ranges(peaks, max_distance_from_peak))}
+    calling_assignments = dict()
+    for ca in cas:
+        assigned_alleles = dict()
+        for a in ca.genotypes.alleles:
+            for p in peaks_by_range:
+                if a in peaks_by_range[p]:
+                    assigned_alleles[a] = p
+                    break
+            else:
+                pass  # TODO: allele was not assigned to a window, consider exception
+        calling_assignments[ca] = assigned_alleles
+    return calling_assignments
