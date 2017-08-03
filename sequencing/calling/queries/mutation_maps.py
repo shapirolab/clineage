4365084312bd20a1cba6222e8224387c1df36a99
ofirr <ofir.raz@weizmann.ac.il>
2017-08-03 14:58:20 +0300

    Added reads_threshold kwarg to get_mono_mutations_dict

--- sequencing/calling/queries/mutation_maps.py
+++ sequencing/calling/queries/mutation_maps.py
@@ -1,13 +1,14 @@
 from sequencing.calling.models import CalledAlleles
 
 
-def get_mono_mutations_dict(srs, calling_scheme, confidence_threshold=0.01):
+def get_mono_mutations_dict(srs, calling_scheme, confidence_threshold=0.01, reads_threshold=30):
     d = dict()
     for sr in srs:
         d[sr] = dict()
         for ca in CalledAlleles.objects.filter(
                 calling_scheme=calling_scheme,
-                histogram__sample_reads=sr).select_subclasses():
+                histogram__sample_reads=sr,
+                histogram__num_reads__gte=reads_threshold).select_subclasses():
             if ca.confidence > confidence_threshold:
                 continue
             d[sr][ca.microsatellite] = ca.genotypes.allele1
