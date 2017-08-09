e90a753f3415c20f371b0ad9e86e4ab9d78c96e6
ofirr <ofir.raz@weizmann.ac.il>
2017-08-09 18:11:27 +0300

    Added tree assessment package
    * Currently with only the triplet metric against known topology

---
+++ sequencing/phylo/tree_assessment/triplets_with_reference.py
@@ -0,0 +1,46 @@
+import itertools
+from sequencing.phylo.tree_assessment.utils import memory_expensive_random_choose
+from dendropy import SeedNodeDeletionException
+from frogress import bar
+
+def challenge_triplets_generator(ref_tree, n=1000, min_d=5):
+    nodes = [n.taxon for n in ref_tree.leaf_nodes()]
+    ndm = ref_tree.node_distance_matrix()
+    pdm = ref_tree.phylogenetic_distance_matrix()
+    for nodes_triplet in memory_expensive_random_choose(nodes, 3, n=n):
+        ref_triplet = ref_tree.extract_tree_with_taxa(nodes_triplet)
+        pairs_in_triplet = list(itertools.combinations(nodes_triplet,2))
+        ref_close_pair = set(sorted(pairs_in_triplet, key=lambda x: pdm.path_edge_count(x[0],x[1]))[0])
+        d = ndm.distance(ref_tree.mrca(taxa=nodes_triplet), ref_tree.mrca(taxa=ref_close_pair))
+        if d < min_d:
+            continue
+        taxon_a, taxon_b = ref_close_pair
+        distance_a_to_ab_mrca = ndm.distance(ref_tree.find_node_for_taxon(taxon_a), ref_tree.mrca(taxa=ref_close_pair))
+        distance_b_to_ab_mrca = ndm.distance(ref_tree.find_node_for_taxon(taxon_b), ref_tree.mrca(taxa=ref_close_pair))
+        if min(distance_a_to_ab_mrca, distance_b_to_ab_mrca) < 2:
+            continue
+        yield d, nodes_triplet, ref_close_pair
+
+
+def check_triplet(tree, nodes_triplet, ref_close_pair):
+    try:
+        rec_triplet = tree.extract_tree_with_taxa(nodes_triplet)
+    except SeedNodeDeletionException:
+        return None
+    if len(rec_triplet.leaf_nodes()) < 3:
+        return None
+    rec_pdm = rec_triplet.phylogenetic_distance_matrix()
+    rec_pdm = tree.phylogenetic_distance_matrix()
+    pairs_in_triplet = list(itertools.combinations(nodes_triplet,2))
+    rec_close_pair = set(sorted(pairs_in_triplet, key=lambda x: rec_pdm.path_edge_count(x[0],x[1]))[0])
+    return ref_close_pair == rec_close_pair
+
+
+def triplets_score(tree_in_question, reference_tree, n=10000, min_d=3):
+    results_by_difficulty=dict()
+    for d, nodes_triplet, ref_close_pair in bar(challenge_triplets_generator(reference_tree, n=n, min_d=min_d)):
+        check_status = check_triplet(tree_in_question, nodes_triplet, ref_close_pair)
+        if d not in results_by_difficulty:
+            results_by_difficulty[d] = {True: 0, False: 0, None: 0}
+        results_by_difficulty[d][check_status] += 1
+    return results_by_difficulty
---
+++ sequencing/phylo/tree_assessment/utils.py
@@ -0,0 +1,28 @@
+import random
+import numpy
+from itertools import combinations
+
+
+def subdict(bigdict, subset_keys):
+    intersection_keys = bigdict.keys() & set(subset_keys)
+    return {k: bigdict[k] for k in intersection_keys}
+
+
+def random_choose(l, k):
+    "Randomized itertools.combinations(iterable, r)"
+    vs = list(l)
+    random.shuffle(vs)
+    for comb in combinations(vs, k):
+        yield comb
+
+
+def memory_expensive_random_choose(l, k, n=50000000):
+    cvs = set()
+    for elm in numpy.random.choice(list(l),(n,k),replace=True):
+        elm = tuple(elm)
+        if len({*elm}) != k:
+            continue
+        if elm in cvs:
+            continue
+        cvs.add(elm)
+        yield elm
