81b994515b466ef651eeb8da3ea1f9ad88ba7b8d
ofirr <ofir.raz@weizmann.ac.il>
2017-08-09 18:45:57 +0300

    Added query for bi allelic calling

--- sequencing/calling/queries/mutation_maps.py
+++ sequencing/calling/queries/mutation_maps.py
@@ -1,4 +1,8 @@
+from targeted_enrichment.planning.models import Microsatellite
+from frogress import bar
+from sequencing.calling.simcor.calling import split_genotypes
 from sequencing.calling.models import CalledAlleles
+from sequencing.analysis.models import Histogram
 import numpy as np
 
 
@@ -34,3 +38,29 @@ def get_mono_mutations_dict(srs, calling_scheme, confidence_threshold=0.01, read
             d[sr][ca.microsatellite] = ca.genotypes.allele1
     return d
 
+
+def map_amplicons_to_ms(srs):
+    # Map the amplicons to microsatellites so that we can filter histograms by ms
+    panels = set(sr.library.subclass.panel for sr in srs)
+    assert len(panels) == 1
+    panel = panels.pop()
+    amp_by_ms = dict()
+    for amp in panel.amplicon_collection.amplicons.all():
+        for ms in Microsatellite.objects.filter(slice__contained=amp.slice):
+            amp_by_ms[ms] = amp
+    return amp_by_ms
+
+
+def get_bi_mutations_dict(srs, calling_scheme, confidence_threshold=0.01, reads_threshold=30, max_distance_from_peak=3,
+                          ms_repaet_unit='AC', histogram_class=Histogram):
+    amp_by_ms = map_amplicons_to_ms(srs)
+    ms_split_calling_results = dict()
+    for ms, amp in bar(amp_by_ms.items()):
+        if ms.repeat_unit_type != ms_repaet_unit:
+            continue
+        ms_split_calling_results[ms] = split_genotypes(ms, srs, amp, calling_scheme,
+                                                       max_distance_from_peak=max_distance_from_peak,
+                                                       confidence=confidence_threshold,
+                                                       reads_threshold=reads_threshold,
+                                                       histogram_class=histogram_class)
+    return ms_split_calling_results
