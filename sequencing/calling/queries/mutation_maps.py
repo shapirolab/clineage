a7dd3f32100bb336a491c537eaa6857cf8c07b74
ofirr <ofir.raz@weizmann.ac.il>
2017-08-13 12:47:17 +0300

    Fixed comparison in filter_mutation_map
    Now includes elements equal to percentile threshold

--- sequencing/calling/queries/mutation_maps.py
+++ sequencing/calling/queries/mutation_maps.py
@@ -20,8 +20,8 @@ def filter_mutation_map(mutations_dict, pl1=50, pl2=50):
     l2_counts = {kl2: len(trans_mutations_dict[kl2]) for kl2 in trans_mutations_dict}
     l1_lim = np.percentile(list(l1_counts.values()), pl1)
     l2_lim = np.percentile(list(l2_counts.values()), pl2)
-    filtered_by_l1 = {kl1: d for kl1, d in mutations_dict.items() if l1_counts[kl1] > l1_lim}
-    filtered_by_l1_l2 = transpose_dict({kl2: d for kl2, d in transpose_dict(filtered_by_l1).items() if l2_counts[kl2] > l2_lim})
+    filtered_by_l1 = {kl1: d for kl1, d in mutations_dict.items() if l1_counts[kl1] >= l1_lim}
+    filtered_by_l1_l2 = transpose_dict({kl2: d for kl2, d in transpose_dict(filtered_by_l1).items() if l2_counts[kl2] >= l2_lim})
     return filtered_by_l1_l2
 
 
