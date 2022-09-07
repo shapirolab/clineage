0a353696d8d64e49c026c181c04c92bf0aec1b93
yaaraneumeier <55943058+yaaraneumeier@users.noreply.github.com>
2022-09-07 13:23:15 +0300

    Update entrypoint_clineage_worker.sh

--- dockerization/resources/entrypoint_clineage_worker.sh
+++ dockerization/resources/entrypoint_clineage_worker.sh
@@ -2,4 +2,4 @@
 set -e
 
 . $(which activate) py36
-PYTHONPATH='/clineage' DJANGO_SETTINGS_MODULE='clineage.settings' dask-worker --nthreads 1 --memory-limit 300000000000 --preload /clineage/worker-setup.py task-scheduler:8786
+PYTHONPATH='/clineage' DJANGO_SETTINGS_MODULE='clineage.settings' dask-worker --nthreads 1 --memory-limit 300000000000 --no-bokeh --preload /clineage/worker-setup.py task-scheduler:8786
