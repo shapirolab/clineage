ed990c94dca3802a6411bdf84b5f24e9a57340e9
Amos Onn <amosonn@gmail.com>
2023-02-07 17:53:22 +0100

    Add version to docker image name clineage:0.0.3

--- dockerization/build.sh
+++ dockerization/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-DOCKER_BUILDKIT=1 docker build -t clineage .
+DOCKER_BUILDKIT=1 docker build -t clineage:0.0.3 .
--- dockerization/run.sh
+++ dockerization/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-docker run --rm -p 8888:80 -u root --entrypoint /bin/bash -it --network=host clineage  
+docker run --rm -p 8888:80 -u root --entrypoint /bin/bash -it --network=host clineage:0.0.3
